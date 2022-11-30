971848f215ab5224cb7ab47258d26f03b4653290
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-11-30 13:58:22 +0000

    Wed, Nov 30, 2022, 1:58 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+// Declaration of the variable "n"
+   int n;
+// Prompt user for pyramid height
+// Ask for height when n is lesser than 1 or greater than 8
+// Print height of the first pyramid
+// Print spaces of the first pyramid
+// Print space between the two pyramids
+// Print height of second pyramid
 }
