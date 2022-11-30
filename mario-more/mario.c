606d6fc4a9d03181cb5715b4ed93466239a79d3a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-11-30 14:01:37 +0000

    Wed, Nov 30, 2022, 2:01 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -6,9 +6,16 @@ int main(void)
 // Declaration of the variable "n"
    int n;
 // Prompt user for pyramid height
+    do
+    {
+        n = get_int("Height: \n");
+    }
+    while (n < 1 || n > 8);
 // Ask for height when n is lesser than 1 or greater than 8
-// Print height of the first pyramid
+// Height of first pyramid
 // Print spaces of the first pyramid
+// Print hashes of the first pyramid
 // Print space between the two pyramids
-// Print height of second pyramid
+// Height of second pyramid
+// Print hashes of second pyramid
 }
