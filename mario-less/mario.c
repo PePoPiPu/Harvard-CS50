940489f58369f5cfcfa838417c1de781640086f2
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-11-30 13:04:39 +0000

    Wed, Nov 30, 2022, 1:04 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -8,12 +8,12 @@ int main(void)
 // Prompt user for pyramid width
     do
     {
-        n = get_int ("Height: \n");
+        n = get_int("Height: \n");
     }
 // While n is less than 1 OR greater than 8
     while (n < 1 || n > 8);
 // Height
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
 // Printing the spaces
         for (int j = n - 1; j > i; j--)
@@ -23,7 +23,7 @@ int main(void)
 // Printing the hashes
         for (int j = 0; j <= i; j++)
         {
-            printf ("#");
+            printf("#");
         }
 // Escape sequence
         printf("\n");
