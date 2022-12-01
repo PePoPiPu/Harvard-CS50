df22171aa772a1256a778d28bce1f06a499d2876
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-01 08:40:08 +0000

    Thu, Dec 1, 2022, 8:40 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -23,7 +23,7 @@ int main(void)
 
     // Calculate the number of dimes to give the customer
     int dimes = calculate_dimes(cents);
-    dimes = quarters - cents/10;
+    dimes = cents - dimes * 10;
 
     // Calculate the number of nickels to give the customer
     int nickels = calculate_nickels(cents);
