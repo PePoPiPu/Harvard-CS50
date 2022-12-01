73049ee2d66578247c7b3e533dd156655bbd7278
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-01 09:44:56 +0000

    Thu, Dec 1, 2022, 9:44 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,8 +6,14 @@ int main(void)
 // Prompt the user for credit card number
     get_long("Type your credit card number: ");
 // Detect type of credit card
-    // American Express: 15 digiits. 34 or 37
+    // American Express: 15 digits. 34 or 37
     // MasterCard: 16 digits. 51, 52, 53, 54, 55
     // Visa: 13 or 16 digits. 4
+
+//Check Sum
+    // Multiply every other digit by 2, starting with second-to-last digit
+    // Add those products digits together
+    // Add the sum to the sum of the digits that weren't multipliead by 2
+    // Is it valid? Total's last digit is 0
 // Print American Express, Mastercard or Visa
 }
