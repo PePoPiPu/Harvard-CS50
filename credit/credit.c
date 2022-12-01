7da929bbf7ede4b1b92d49b68b4b2da32b4bce1b
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-01 09:40:17 +0000

    Thu, Dec 1, 2022, 9:40 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,5 +5,7 @@ int main(void)
 {
 // Prompt the user for credit card number
     get_long("Type your credit card number: ");
-//Print American Express, Mastercard or Visa
+// Detect type of credit card
+    do g
+// Print American Express, Mastercard or Visa
 }
