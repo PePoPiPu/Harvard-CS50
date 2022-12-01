7eb244e7f4deb5ba6c7c61fad68724d43b74d68e
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-01 09:07:04 +0000

    Thu, Dec 1, 2022, 9:07 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -30,19 +30,45 @@ int main(void)
 
         return quarters;
     }
-    int quarters = calculate_quarters(cents);
-    // Calculate the number of dimes to give the customer
-    int dimes = calculate_dimes(cents);
-   cents = cents - dimes * 10;
+// Calculate the number of dimes to give the customer
+    int calculate_dimes(int cents);
+    {
+        int dimes = 0;
 
-    // Calculate the number of nickels to give the customer
-    int nickels = calculate_nickels(cents);
-    cents = cents - nickels * 5;
+        while (cents >= 10)
+        {
+            dimes++;
+            cents = cents - 10;
+        }
 
-    // Calculate the number of pennies to give the customer
-    int pennies = calculate_pennies(cents);
-    cents = cents - pennies * 1;
+        return dimes;
+    }
+// Calculate the number of nickels to give the customer
+    int calculate_nickels(int cents);
+    {
+            int nickels = 0;
 
+            while (cents >= 5)
+            {
+                nickels++;
+                cents = cents - 5;
+            }
+
+            return nickels;
+    }
+// Calculate the number of pennies to give the customer
+    int calculate_pennies(int cents);
+    {
+        int pennies = 0;
+
+        while (cents >= 1)
+        {
+            pennies++;
+            cents = cents -1;
+        }
+
+        return pennies;
+    }
     // Sum coins
     int coins = quarters + dimes + nickels + pennies;
 
