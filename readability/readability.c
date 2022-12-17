6c886c199b8fad14948861bfcf7ff4622f8f2f07
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-17 22:35:34 +0000

    Sat, Dec 17, 2022, 10:35 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
+#include <math.h>
 
 int main(void)
 {
@@ -10,34 +10,28 @@ int main(void)
     printf("%s\n", text);
 
     // Counting the letters of the text
-
     int letters = 0;
     for(int i = 0; i < strlen(text); i++)
     {
         if((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
         letters++;
     }
-    printf("%i letters\n", letters);
 
     // Counting the number of words
-
     int words = 1;
     for(int i = 0; i < strlen(text); i++)
     {
         if (text[i] == ' ')
         words++;
     }
-    printf("%i words\n", words);
 
     // Counting the number of sentences
-
     int sentences = 0;
     for(int i = 0; i < strlen(text); i++)
     {
         if (text[i] == '.' || text[i] == '?' || text[i] == '!')
         sentences++;
     }
-    printf("%i sentences\n", sentences);
 
     // Determining the reading grade level using the Coleman - Liau index
     float calculation = (0.0588 * letters / words * 100) - (0.296 * sentences / words * 100) - 15.8;
@@ -49,7 +43,7 @@ int main(void)
     }
     else if (index >= 16)
     {
-        prinf("Grade 16+\n");
+        printf("Grade 16+\n");
         return 0;
     }
     else
