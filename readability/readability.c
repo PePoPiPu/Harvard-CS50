e089a45b564a568afa5cc8459982aaa50f5f74d8
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-17 21:46:59 +0000

    Sat, Dec 17, 2022, 9:46 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,20 +8,7 @@ int count_letters(string text);
 float l;
 int main(void)
 {
-    string s = get_string("Text: ");
-    int letters = count_letters(s);
-    printf("%s\n");
+    string text = get_string("Text: ");
+    printf("%s\n", text);
 }
 
-int count_letters(string text)
-{
-    int chars = 0;
-    for(int i = 0; text[i] != '\0' ; i++)
-    {
-        if (isalpha[i])
-        {
-            chars++
-        }
-    }
-    return chars;
-}
