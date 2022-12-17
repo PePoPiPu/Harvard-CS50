9cbafc89a3d1011336cfb9bb6ae6f57859092d20
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-17 21:54:37 +0000

    Sat, Dec 17, 2022, 9:54 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,12 +3,22 @@
 #include <ctype.h>
 #include <string.h>
 
-int count_letters(string text);
-
-float l;
 int main(void)
 {
+    // Prompt user for text
     string text = get_string("Text: ");
     printf("%s\n", text);
+
+    // Counting the letters of the text
+
+    int letters = 0;
+    for(int i = 0; i < strlen(text); i++)
+    {
+        if((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+        letters++;
+    }
+    printf("%i letters", letters);
+    printf("\n");
 }
+
 
