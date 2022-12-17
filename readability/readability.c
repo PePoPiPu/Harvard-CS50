fc3a17b1bfbc7f324899840c93549f8c2ee149c7
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-17 20:33:22 +0000

    Sat, Dec 17, 2022, 8:33 PM +00:00

---
+++ readability/readability.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+int count_letters(string text)
+
+int main(void)
+{
+    string s = get_string("Text: ");
+    printf("%s\n", s);
+}
+
+int count_letters(string text)
+{
+    for
+}
