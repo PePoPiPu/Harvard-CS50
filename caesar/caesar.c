5925f120418d4edefdf33a64da33ac9e160b9a97
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-17 23:15:49 +0000

    Sat, Dec 17, 2022, 11:15 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,7 +5,7 @@ int main(int argc, string argv[])
 {
     if(argc != 2)
     {
-        printf("Please input cipher Key\n");
+        printf("Please input a single command-line argument\n");
         return 1;
     }
     printf("hello, %s\n", argv[1]);
