5b4fc59cf189b61dddafb28f5cda2518bbed465b
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-17 23:08:09 +0000

    Sat, Dec 17, 2022, 11:08 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,5 +3,12 @@
 
 int main(int argc, string argv[])
 {
-    printf("hello, %s\n", argv[0]);
+    if(argc == 2)
+    {
+        printf("Hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("hello, world\n");
+    }
 }
