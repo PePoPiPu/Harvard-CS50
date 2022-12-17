4b69854cbf82d01b12cf32dc101ba5888c5f35c1
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-17 23:17:48 +0000

    Sat, Dec 17, 2022, 11:17 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,9 +5,8 @@ int main(int argc, string argv[])
 {
     if(argc != 2)
     {
-        printf("Please input a single command-line argument\n");
+        printf("Usage: ./caesar key\n");
         return 1;
     }
-    printf("hello, %s\n", argv[1]);
     return 0;
 }
