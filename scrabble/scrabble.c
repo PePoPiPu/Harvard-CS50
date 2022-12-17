8ff5691a8dfe730dbe4571db6240a04f1238c39f
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-17 14:53:50 +0000

    Sat, Dec 17, 2022, 2:53 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,9 +19,17 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
-    if
+    if (score1 < score2)
     {
-        score1 < score2 
+        printf ("Player 2 Wins!\n");
+    }
+    if (score1 > score2)
+    {
+        printf ("Player 1 Wins!\n");
+    }
+    if (score1 == score2)
+    {
+        printf ("Tie!\n");
     }
 }
 
