faac6017c8da604846a71453dd51a9caf1f5ec3b
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-18 00:10:54 +0000

    Sun, Dec 18, 2022, 12:10 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,11 +6,13 @@ bool only_digits(string s);
 
 int main(int argc, string argv[])
 {
+    // Check if program was run with one command-line argument
     if(argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
+    // Checking if every character in arv[1] is a digit
     if(isdigit(argc))
     {
         return 0;
