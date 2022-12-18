038b792071aa79a07bb1616f3910c3a3b76d46be
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-18 01:01:45 +0000

    Sun, Dec 18, 2022, 1:01 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -23,6 +23,18 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    // Check if 
+    // Check if argv[1] is an integer, if not print error message and return 1
+    int j = atoi(argv[1]);
+
+    if(isdigit(j) == 0)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    else
+    {
+        puts("Success");
+        printf("%s\n", argv[1]);
+    }
 }
 
