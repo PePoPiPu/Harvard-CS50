643cd15181a79da72334ffe9e8cd3ae565a7c88d
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-18 01:29:51 +0000

    Sun, Dec 18, 2022, 1:29 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -13,19 +13,18 @@ int main(int argc, string argv[])
     if(argc != 2)
     {
         printf("Usage: ./caesar key\n");
-        return 1;
     }
 }
 
 bool only_digits(string s)
 {
-    for (int i = 0; length = strlen(s); i < length; i++)
+    for (int i = 0, length = strlen(s);i < length; i++)
     {
         if (!isdigit(s[i]))
         {
             return false;
         }
-        return true;
     }
+    return true;
 }
 
