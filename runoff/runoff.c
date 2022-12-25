0a8ea5471d94020911dc4dc64185d244b40a2e99
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-25 16:48:10 +0000

    Sun, Dec 25, 2022, 4:48 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
     // TODO
     if (strcmp(candidates[i].name, name) == 0)
     {
-        preferences[][]
+        preferences[i][j]
     }
     return false;
 }
