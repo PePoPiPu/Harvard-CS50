88c82276e7e75f8e1a8823731bd2fa87b9d319e4
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-25 19:42:54 +0000

    Sun, Dec 25, 2022, 7:42 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -162,10 +162,13 @@ void tabulate(void)
 bool print_winner(void)
 {
     // TODO
-    if (candidates[i].votes > voter_count/2)
+    for (int i = 0; i < candidate_count; i++)
     {
-        printf ("%s\n", candidates[i].name);
-        return true;
+        if (candidates[i].votes > voter_count/2)
+        {
+            printf ("%s\n", candidates[i].name);
+            return true;
+        }
     }
     return false;
 }
