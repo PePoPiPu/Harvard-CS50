4597978750c1f307b1e62e2444a9c4474961aba7
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-25 16:22:23 +0000

    Sun, Dec 25, 2022, 4:22 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -98,7 +98,7 @@ void print_winner(void)
     {
         if (candidates[i].votes == maximum_vote)
         {
-            printf("%s\n", candidates[i].name);
+            printf("The winner is: %s\n", candidates[i].name);
         }
     }
     return;
