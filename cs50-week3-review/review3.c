702f40fc25d9b0c87b09dd4a64dcc639db2e25d2
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-26 15:03:19 +0000

    Mon, Dec 26, 2022, 3:03 PM +00:00

--- cs50-week3-review/review3.c
+++ cs50-week3-review/review3.c
@@ -3,6 +3,10 @@
 #include <string.h>
 // This is a review of structs
 
+int main (void)
+{
+    
+}
 
 
 
@@ -16,19 +20,19 @@
 
 
 // Correct program but bad designed. We would have to carefully maintain each array. Bad when having a lot of data.
-int main (void)
-{
-    string names[] = {"Carter", "David"};
-    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
+//int main (void)
+//{
+//    string names[] = {"Carter", "David"};
+//    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
 
-    for (int i = 0; i < 2; i++)
-    {
-        if (strcmp(names[i], "David") == 0)
-        {
-            printf("Found %s\n", numbers[i]);
-            return 0;
-        }
-    }
-    printf("Not found\n");
-    return 1;
-}
+//    for (int i = 0; i < 2; i++)
+//    {
+//        if (strcmp(names[i], "David") == 0)
+//        {
+//            printf("Found %s\n", numbers[i]);
+//            return 0;
+//        }
+//    }
+//    printf("Not found\n");
+//    return 1;
+//}
