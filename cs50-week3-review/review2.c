aa6b92b975547fb036c3770d09db33758589f38e
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-26 14:44:09 +0000

    Mon, Dec 26, 2022, 2:44 PM +00:00

--- cs50-week3-review/review2.c
+++ cs50-week3-review/review2.c
@@ -9,14 +9,14 @@ int main (void)
     for (int i = 0; i < 7; i++)
     {
         // Correct string comparison using strcmp. Returns a negative value if the first string comes before the second one, a positive value if it comes after and a 0 if it's the same string.
-        if (strcmp(names[i], "Bill") == 0)
+        if (strcmp(names[i], "Ron") == 0)
         {
             printf("Found\n");
             return 0;
         }
+    }
     printf ("Not found\n");
     return 1;
-    }
 }
 
 // Example of an incorrect conditional. Trying to compare a string as an integer returns an error.
