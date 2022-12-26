f0741eb98951ac14fbf114fe5a179834d5dd5c8f
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-26 15:11:15 +0000

    Mon, Dec 26, 2022, 3:11 PM +00:00

--- cs50-week3-review/review3.c
+++ cs50-week3-review/review3.c
@@ -3,11 +3,36 @@
 #include <string.h>
 // This is a review of structs
 
+// We can create a data type called person that holds 2 arrays
+// Struct definitions go before MAIN, the same with function prototypes
+typedef struct // Telling the compiler that we're defining our own data structure.
+{
+    string name;
+    string number;
+}
+person; // Telling the compiler the name of the data structure.
+
 int main (void)
 {
-    
-}
+    person people[2];
+// Setting the values for each variable inside each person struct. We use the DOT operator.
+    people[0].name = "Carter";
+    people[0].number = "+1-617-495-1000";
 
+    people[1].name = "David";
+    people[1].number = "+1-949-468-2750";
+
+    for (int i = 0; i < 2; i++)
+    {
+        if(strcmp(people[i].name, "David") == 0) // Accessing the "name" variable of the struct.
+        {
+            printf("Found %s\n", people[i].number); //Accessing the "number" variable of the struct.
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
 
 
 
