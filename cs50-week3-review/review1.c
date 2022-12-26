fd6d0414a459640c689b53f48f8b34531e664ba6
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-26 14:29:20 +0000

    Mon, Dec 26, 2022, 2:29 PM +00:00

--- cs50-week3-review/review1.c
+++ cs50-week3-review/review1.c
@@ -3,17 +3,20 @@
 
 int main (void)
 {
+    // Initializing the array
     int numbers[] = {4, 6, 8, 2, 7, 5, 0};
     // Initializing for loop, index 0 (1st position in the array), while the index is less than 7th position, count up once.
     for (int i = 0; i < 7; i++)
     {
     // If one of the numbers in the array equals 0, print and return and exit status of 0
-        if (numbers[i] == 0)
+        if (numbers[i] == -1)
         {
             printf("Found\n");
+    // Exit code of 0: Indicates success.
             return 0;
         }
     }
     printf("Not found\n");
+    // Exit code of 1: Error code
     return 1;
 }
