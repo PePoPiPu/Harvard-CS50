5c29bef563e048165a6eb4bd291a72ddba9744be
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 01:05:01 +0000

    Tue, Dec 27, 2022, 1:05 AM +00:00

--- cs50-week4-review/pointer.c
+++ cs50-week4-review/pointer.c
@@ -3,8 +3,8 @@
 int main (void)
 {
     char *s = "HI!";
-    printf("%c\n", *s); //Go directly to the address stored in s
-    printf("%c\n", *(s + 1));// Go directly to the address stored in s and go to the next location in memory, a byte higher.
+    printf("%c\n", *s); //Go directly to the address stored in s and get the stored value (a char).
+    printf("%c\n", *(s + 1));// Go directly to the address stored in s and go to the next location in memory, a byte higher. Get the stored value (a char).
     printf("%c\n", *(s + 2));
 }
 
