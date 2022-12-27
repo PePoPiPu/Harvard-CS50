425cec36fb7e62d12ff30f410f025c7321258af6
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 18:28:48 +0000

    Tue, Dec 27, 2022, 6:28 PM +00:00

--- cs50-week4-review/scanf3.c
+++ cs50-week4-review/scanf3.c
@@ -3,7 +3,7 @@
 //Example of a program that gives a segmentation fault.
 int main (void)
 {
-    // The program works safely as long as we input a string less than 4 characters long. Higher than that, we try to touch memory that we haven't allocated, giving us a segmentation fault.
+    // The program works safely as long as we input a string less than 4 characters long. Higher than that, we try to touch memory that we haven't allocated, giving us a segmentation fault. 
     char s[4];
     printf("s: ");
     scanf("%s", s);
