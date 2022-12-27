0496354357a8f0d921253e4111efa68d29aaf91a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 00:22:08 +0000

    Tue, Dec 27, 2022, 12:22 AM +00:00

---
+++ cs50-week4-review/address2.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    char *s = "HI!";
+    printf("%s\n", s);
+}
+
+
+
+
+
+
+
+
+// Example of use of string data type, include in the CS50 library
+//int main (void)
+//{
+//    string s = "HI!";
+//    printf("%s\n", s);
+//}
