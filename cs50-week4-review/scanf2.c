47f1a2e1f2fb871c5572819151e6ecfa5c0ea3cb
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 18:17:41 +0000

    Tue, Dec 27, 2022, 6:17 PM +00:00

--- cs50-week4-review/scanf2.c
+++ cs50-week4-review/scanf2.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (void)
 {
-    char *s;
+    char *s = malloc(4);
     printf("s: ");
     scanf("%s", s);
     printf("s: %s\n", s);
 }
+
+
+
+//Example of erroneous program
+//int main (void)
+//{
+//    char *s;
+//    printf("s: ");
+//Scanf is writing our string to an unknown address in memory since we didn't allocate any.
+//    scanf("%s", s);
+//    printf("s: %s\n", s);
+//}
