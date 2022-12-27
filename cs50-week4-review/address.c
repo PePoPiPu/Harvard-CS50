2eb1b4bd96587098f5daa24544c1c19b06ecdfaa
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 00:13:13 +0000

    Tue, Dec 27, 2022, 12:13 AM +00:00

--- cs50-week4-review/address.c
+++ cs50-week4-review/address.c
@@ -4,5 +4,5 @@ int main (void)
 {
     int n = 50;
     int *p = &n;
-    printf("%p\n", p);
+    printf("%p\n", p); //%p os the format code to print an adress. After it we need to use the name of the variable we have created, p.
 }
