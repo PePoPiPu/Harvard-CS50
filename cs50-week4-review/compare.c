a0451042ceef18e9bb9ba20c076f8332f1da33d9
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 12:56:28 +0000

    Tue, Dec 27, 2022, 12:56 PM +00:00

--- cs50-week4-review/compare.c
+++ cs50-week4-review/compare.c
@@ -1,12 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main (void)
 {
-    char *s = get_string("s: ");
-    char *t = get_string("t: ");
+    char *s = get_string ("s: ");
+    char *t = get_string ("t :");
 
-    if (s == t)
+    if (strcmp (s, t) == 0)
     {
         printf("Same\n");
     }
@@ -24,6 +25,22 @@ int main (void)
 
 
 
+
+// Example of incorrect comparison between strings
+//int main (void)
+//{
+//    char *s = get_string("s: "); //Each char * POINTS to a different location in memory where the first character of the string is stored.
+//    char *t = get_string("t: "); //Even if the characters are exactly the same, they exist in different points of memory, thus, being different.
+
+//    if (s == t)
+//    {
+//        printf("Same\n");
+//    }
+//    else
+//    {
+//        printf("Different\n");
+//    }
+//}
 
 // Basic program that compares 2 integers
 //int main (void)
