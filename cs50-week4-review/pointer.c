32ead9fdb2a4d06ca8ae6b6e961ce0bf13920644
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 01:01:49 +0000

    Tue, Dec 27, 2022, 1:01 AM +00:00

--- cs50-week4-review/pointer.c
+++ cs50-week4-review/pointer.c
@@ -3,7 +3,19 @@
 int main (void)
 {
     char *s = "HI!";
-    printf("%c\n", s[0]);
-    printf("%c\n", s[1]);
-    printf("%c\n", s[2]);
+    printf("%c\n", *s);
+    printf("%c\n", *(s + 1));
+    printf("%c\n", *(s + 2));
 }
+
+
+
+
+// An example of syntactic sugar. This way is easier to write and read by a human
+//int main (void)
+//{
+//    char *s = "HI!";
+//    printf("%c\n", s[0]);
+//    printf("%c\n", s[1]);
+//    printf("%c\n", s[2]);
+//}
