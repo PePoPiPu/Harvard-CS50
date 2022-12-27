14934bda0f85601a9204aba8c5181eb837d73e2e
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 12:51:48 +0000

    Tue, Dec 27, 2022, 12:51 PM +00:00

--- cs50-week4-review/compare.c
+++ cs50-week4-review/compare.c
@@ -3,10 +3,10 @@
 
 int main (void)
 {
-    int i = get_int("i: ");
-    int j = get_int("j: ");
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
 
-    if (i == j)
+    if (s == t)
     {
         printf("Same\n");
     }
@@ -15,3 +15,28 @@ int main (void)
         printf("Different\n");
     }
 }
+
+
+
+
+
+
+
+
+
+
+// Basic program that compares 2 integers
+//int main (void)
+//{
+//    int i = get_int("i: ");
+//    int j = get_int("j: ");
+
+//    if (i == j)
+//    {
+//        printf("Same\n");
+//    }
+//    else
+//    {
+//        printf("Different\n");
+//    }
+//}
