2a64ce9fd024792146fe43df086e9776bbfd6270
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 18:13:38 +0000

    Tue, Dec 27, 2022, 6:13 PM +00:00

--- cs50-week4-review/scanf.c
+++ cs50-week4-review/scanf.c
@@ -4,6 +4,7 @@ int main (void)
 {
     int x;
     printf("x: ");
+//"Scans" the input for the format in the placeholder. Then it passes the address in memory where we want that into put to go. In this case, we want it to go to a variable called x.
     scanf("%i", &x);
     printf("x: %i\n",x);
 }
