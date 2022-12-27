18823491781f589681041b684ced469aaffcac5c
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 00:16:20 +0000

    Tue, Dec 27, 2022, 12:16 AM +00:00

--- cs50-week4-review/address.c
+++ cs50-week4-review/address.c
@@ -5,5 +5,5 @@ int main (void)
     int n = 50;
     int *p = &n;
     printf("%p\n", p); //%p os the format code to print an adress. After it we need to use the name of the variable we have created, p.
-    printf("%i\n", *p);
+    printf("%i\n", *p);//In this case,* is a dereference operator. Goes to the address in p to get the value stored there.
 }
