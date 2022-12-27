647aed9d4ed239d8f5fb071955a4434267e95a44
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 13:28:06 +0000

    Tue, Dec 27, 2022, 1:28 PM +00:00

--- cs50-week4-review/memory.c
+++ cs50-week4-review/memory.c
@@ -3,8 +3,13 @@
 
 int main (void)
 {
+//Getting enough memory for 3 times the size of an int (4 x 3 = 12 bytes)
     int *x = malloc(3 * sizeof(int));
+//Starting an array with 1 instead of 0 (Remember that all arrays are 0 indexed). We are trying to access memory beyond the bounds of what we have access to.
     x[1] = 72;
     x[2] = 73;
     x[3] = 33;
+//We didn't free the memory we allocated.
 }
+
+//These mistakes aren't bad enough here but in a real setting, it might cause a segmentation fault.
