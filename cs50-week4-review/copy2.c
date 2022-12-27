16633f256823047bde721938146ba791c111621c
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-27 13:11:51 +0000

    Tue, Dec 27, 2022, 1:11 PM +00:00

--- cs50-week4-review/copy2.c
+++ cs50-week4-review/copy2.c
@@ -9,7 +9,7 @@ int main (void)
     char *s = get_string("s: ");
     char *t = malloc(strlen(s) + 1); // Allocating memory for s lenght + 1 to leave space for the null character.
 
-    for (int i = 0; n = strlen(s) + 1; i < n; i++) //Copying each character one at a time. We use strlen(s) + 1 to copy the null character too.
+    for (int i = 0, n = strlen(s) + 1; i < n; i++) //Copying each character one at a time. We use strlen(s) + 1 to copy the null character too.
     {
         t[i] = s[i]; //Assigning the characters of s to t
     }
