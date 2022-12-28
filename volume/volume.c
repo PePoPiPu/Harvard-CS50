b62171de7b767d195f7d5794301485fdf5dd573c
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2022-12-28 23:10:02 +0000

    Wed, Dec 28, 2022, 11:10 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,8 +40,8 @@ int main(int argc, char *argv[])
     // Storing the Header into an array, which in itself is a data type of an unsigned integer
     uint8_t header[HEADER_SIZE];
 
-        fread (&(header[HEADER_SIZE]), 1, 1, input);
-        fwrite (&(header[HEADER_SIZE]), 1, 1, output);
+        fread (header, HEADER_SIZE, 1, input);
+        fwrite (header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
     // Storing the samples into an array, which in itself is a data type of signed integer
