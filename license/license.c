a215c86b8f9d4552d0a509e81418a26a3ae8ff69
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-04 18:03:26 +0000

    Wed, Jan 4, 2023, 6:03 PM +00:00

--- license/license.c
+++ license/license.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
         buffer[6] = '\0';
 
         // Save plate number in array
-        plates[idx] = malloc(0);
+        plates[idx] = malloc(7);
         strcpy(plates[idx], buffer);
         idx++;
     }
