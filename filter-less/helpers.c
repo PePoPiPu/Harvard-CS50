ace136e8c54e552e47788cd3a90c781dba6185fd
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-06 10:50:30 +0000

    Fri, Jan 6, 2023, 10:50 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,6 +7,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            int r = image[i][j].rgbtRed;
+            int g = image[i][j].rgbtGreen;
+            int b = image[i][j].rgbtBlue;
 
         }
     }
