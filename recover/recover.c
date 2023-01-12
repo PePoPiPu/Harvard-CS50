5c507d783c6d793f3e547ed0439c7abccecade5a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-12 20:59:48 +0000

    Thu, Jan 12, 2023, 8:59 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+// Function prototypes
+size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +13,7 @@ int main(int argc, char *argv[])
     }
  // Open the memory card
     FILE *card = fopen(arv[1], "r");
+    fread ()
  // Look for the beginning of a JPEG
  // Open a new JPEG file
  // Write 512 bytes until a new JPEG is found
