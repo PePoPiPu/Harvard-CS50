45fe9af12d5ff6bba0ec7a8f8468fa5586f85a81
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-12 16:42:43 +0000

    Thu, Jan 12, 2023, 4:42 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -12,7 +12,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         // Iterating through width pixels
         for (int j = 0; j < width; j++)
         {
-            rgbgray = round ((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3.0);
+            rgbgray = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3.0);
             image[i][j].rgbtRed = rgbgray;
             image[i][j].rgbtGreen = rgbgray;
             image[i][j].rgbtBlue = rgbgray;
@@ -35,9 +35,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Using sepia's algorithm. Casting first values to an int so the programm doesn't run out of bits.
-            redsepia = round (( .393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue) * 1.0);
-            greensepia = round (( .349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue) * 1.0);
-            bluesepia = round (( .272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue) * 1.0);
+            redsepia = round((.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue) * 1.0);
+            greensepia = round((.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue) * 1.0);
+            bluesepia = round((.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue) * 1.0);
             // Assigning sepia values to the original pixel values
             image[i][j].rgbtRed = redsepia;
             image[i][j].rgbtGreen = greensepia;
@@ -67,13 +67,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-            for (int j = 0; j < width / 2 ; j++)
-            {
-               RGBTRIPLE tmp[height][width];
-                tmp[i][j] = image [i][j];
-                image[i][j] = image[i][width - (j + 1)];
-                image[i][width - (j + 1 )] = tmp [i][j];
-            }
+        for (int j = 0; j < width / 2 ; j++)
+        {
+            RGBTRIPLE tmp[height][width];
+            tmp[i][j] = image [i][j];
+            image[i][j] = image[i][width - (j + 1)];
+            image[i][width - (j + 1)] = tmp [i][j];
+        }
     }
     return;
 }
@@ -83,13 +83,13 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     // Create a temporary copy of the image
     RGBTRIPLE temp[height][width];
-        for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
         {
-            for (int j = 0; j < width; j++)
-            {
-                temp [i][j] = image[i][j];
-            }
+            temp [i][j] = image[i][j];
         }
+    }
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j ++)
@@ -101,7 +101,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             // Getting the neighbouring pixels
             for (int x = - 1; x < 2; x++)
             {
-                for ( int y = - 1; y < 2; y++)
+                for (int y = - 1; y < 2; y++)
                 {
                     int currentX = i + x;
                     int currentY = j + y;
