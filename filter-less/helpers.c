5a645e1b336afb1708157dfdee84ef5d77f886a1
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-12 14:22:55 +0000

    Thu, Jan 12, 2023, 2:22 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -34,14 +34,29 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         // Iterating through width pixels
         for (int j = 0; j < width; j++)
         {
-            // Using sepia's algorithm. Casting first values to an int so the program doesn't lose any bits.
+            // Using sepia's algorithm. Casting first values to an int so the programm doesn't run out of bits.
             redsepia = round (((int) 0.393 * image[i][j].rgbtRed) + (0.769 * image[i][j].rgbtGreen) + (0.189 * image[i][j].rgbtBlue));
             greensepia = round (((int) 0.349 * image[i][j].rgbtRed) + (0.686 * image[i][j].rgbtGreen) + (0.168 * image[i][j].rgbtBlue));
             bluesepia = round (((int) 0.272 * image[i][j].rgbtRed) + (0.534 * image[i][j].rgbtGreen) + (0.131 * image[i][j].rgbtBlue));
-            // Stablishing maximum pixel value to 255 so it doesn't fall over that value.
-            redsepia = min (redsepia, 255);
-            greensepia = min (greensepia, 255);
-            bluesepia = min (bluesepia, 255);
+            // Assigning sepia values to the original pixel values
+            image[i][j].rgbtRed = redsepia;
+            image[i][j].rgbtGreen = greensepia;
+            image[i][j].rgbtBlue = bluesepia;
+
+            if (redsepia > 255)
+            {
+                redsepia = 255;
+            }
+
+            if (greensepia > 255)
+            {
+                greensepia = 255;
+            }
+
+            if (bluesepia > 255)
+            {
+                bluesepia = 255;
+            }
         }
     }
     return;
