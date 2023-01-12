47b00ab93bf6e4143464440bf53454fc98f64e60
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-12 20:50:00 +0000

    Thu, Jan 12, 2023, 8:50 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
+ // Open the memory card
  
+ // Look for the beginning of a JPEG
+ // Open a new JPEG file
+ // Write 512 bytes until a new JPEG is found
+ // Stop at the end of the file
 }
