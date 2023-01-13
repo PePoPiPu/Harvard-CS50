3786980f154d82c4019985458f611b42be0411fc
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-13 14:55:50 +0000

    Fri, Jan 13, 2023, 2:55 PM +00:00

---
+++ no-vowels/no-vowels.c
@@ -0,0 +1,16 @@
+// Write a function to replace vowels with numbers
+// Get practice with strings
+// Get practice with command line
+// Get practice with switch
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./no-vowels word\n");
+        return 1;
+    }
+}
