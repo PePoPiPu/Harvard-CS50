1bb432b9fb5c9e695acd3260709ae225a539340a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-13 18:04:59 +0000

    Fri, Jan 13, 2023, 6:04 PM +00:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -23,27 +23,27 @@ string replace (string argv[1])
 
     for (int i = 0; i < sizeof(argv[1]); i++)
     {
-        if(word == 'a')
+        if(word[i] == 'a')
         {
             'a' == '6';
         }
 
-        if(word == 'e')
+        if(word[i] == 'e')
         {
             'e' == '3';
         }
 
-        if(word == 'i')
+        if(word[i] == 'i')
         {
             'i' == '1';
         }
 
-        if(word == 'o')
+        if(word[i] == 'o')
         {
             'o' == '0';
         }
 
-        if(word == 'u')
+        if(word[i] == 'u')
         {
             'u' == 'u';
         }
