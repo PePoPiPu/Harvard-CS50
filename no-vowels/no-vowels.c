eefa2c22cc1bfbd1c5e1e9661ff2458676d02e02
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-13 20:10:19 +0000

    Fri, Jan 13, 2023, 8:10 PM +00:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -2,22 +2,11 @@
 // Get practice with strings
 // Get practice with command line
 // Get practice with switch
+
 #include <cs50.h>
 #include <stdio.h>
-// Function prototypes
-char replace (char argv[i]);
 
-
-int main(int argc, char* argv[])
+int main(int argc, string argv[])
 {
-    // Ensure proper usage
-    if (argc != 2)
-    {
-        printf("Usage: ./no-vowels word\n");
-        return 1;
-    }
-    // 
-    printf("%c\n", replace(argv[1]));
-
+    
 }
-
