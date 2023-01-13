a16a8ac0b4bf8ddcb3a05347d1b95dfa7bfda58a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-13 19:43:31 +0000

    Fri, Jan 13, 2023, 7:43 PM +00:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -7,48 +7,17 @@
 // Function prototypes
 char replace (char argv[i]);
 
+
 int main(int argc, char* argv[])
 {
+    // Ensure proper usage
     if (argc != 2)
     {
         printf("Usage: ./no-vowels word\n");
         return 1;
     }
-
+    // 
     printf("%c\n", replace(argv[1]));
 
 }
 
-char replace (char argv[i])
-{
-    for (int i = 0; i < sizeof(argv[1]); i++)
-    {
-        if(argv[i] == 'a')
-        {
-            argv[i] = '6';
-        }
-
-        if(argv[i] == 'e')
-        {
-            argv[i] = '3';
-        }
-
-        if(argv[i] == 'i')
-        {
-            argv[i] = '1';
-        }
-
-        if(argv[i] == 'o')
-        {
-            argv[i] = '0';
-        }
-
-        if(argv[i] == 'u')
-        {
-            argv[i] = 'u';
-        }
-
-            return argv[i];
-    }
-    return 0;
-}
