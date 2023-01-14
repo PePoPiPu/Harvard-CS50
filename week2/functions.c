f422a6b1fa4a0c6589be96d4f83976a8594d2674
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-14 12:35:23 +0000

    Sat, Jan 14, 2023, 12:35 PM +00:00

--- week2/functions.c
+++ week2/functions.c
@@ -1,7 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 float mult_two_floats (float a, float b);
+int add_two_ints (int a, int b);
 int main (void)
 {
 
+}
+
+float mult_two_floats(float a, float b)
+{
+    float product = a * b;
+    return product;
+}
+
+int add_two_ints (int a, int b)
+{
+    int sum = a + b;
+    return sum;
 }
