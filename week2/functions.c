e2feabc0321e9c6c52fc4f5622cec486cca03e37
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-14 12:40:00 +0000

    Sat, Jan 14, 2023, 12:40 PM +00:00

--- week2/functions.c
+++ week2/functions.c
@@ -4,7 +4,14 @@ float mult_two_floats (float a, float b);
 int add_two_ints (int a, int b);
 int main (void)
 {
+    printf("Give me an integer: \n");
+    int x = GetInt();
+    printf("Give me another integer: \n");
+    int y = GetInt();
 
+    int z = add_two_ints (x, y);
+
+    printf("The sum if %i and %i is %i\n" x, y, z);
 }
 
 float mult_two_floats(float a, float b)
