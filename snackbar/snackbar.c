d8122de429a9b521c9e30c99016b04f7f22b9269
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-14 21:19:06 +0000

    Sat, Jan 14, 2023, 9:19 PM +00:00

--- snackbar/snackbar.c
+++ snackbar/snackbar.c
@@ -100,5 +100,11 @@ void add_items(void)
 // Search through the menu array to find an item's cost
 float get_cost(string item)
 {
+    float sum = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+
+    }
     return 0.0;
 }
