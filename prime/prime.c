e444d5828c090557127a04b9d57f18db8f0e6f86
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-15 11:16:07 +0000

    Sun, Jan 15, 2023, 11:16 AM +00:00

--- prime/prime.c
+++ prime/prime.c
@@ -31,8 +31,7 @@ int main(void)
 bool prime(int number)
 {
     bool is_prime = false;
-    int min = ;
-    for (int i = min; i <= max; i++)
+    for (int i = number; i <= max; i++)
     {
         if((i % i + 1 && i % 1 && i % 2) == 0)
         {
