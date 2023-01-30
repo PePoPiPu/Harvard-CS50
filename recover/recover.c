1664994f32bd80e7017885c1cd99ab959eeeb31a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-30 14:33:47 +0000

    Mon, Jan 30, 2023, 2:33 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -18,21 +18,8 @@ int main(int argc, char *argv[])
     BYTE buffer[512];
 // Declaring filename
     char filename [3];
-
-
-
- // Look for the beginning of a JPEG (0xff, 0xd8, 0xff)
-    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
-    {
-
-        if ((buffer[3] & 0xf0) == 0xe0)
-        {
-            sprintf(filename, "%03i.jpg", 2);
-            FILE *img = fopen(filename, "w");
-            fwrite(nimg *ptr, 512, sizeof(card), FILE *jpeg);
-        }
-    }
- // Open a new JPEG file
- // Write 512 bytes until a new JPEG is found
- // Stop at the end of the file
+// Setting a counter to 0
+    int counter = 0;
+// Start reading file
+    while(fread(buffer,sizeof(BYTE), 512))
 }
