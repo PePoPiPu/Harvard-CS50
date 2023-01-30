d9e4398fcd7ae2e68a8176d23606da51080ad8f5
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-30 16:07:08 +0000

    Mon, Jan 30, 2023, 4:07 PM +00:00

--- week5/list.c
+++ week5/list.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (void)
 {
     int *list = malloc(3 * sizeof(int));
     if (list == NULL)
-    
+    {
+        return 1;
+    }
 
     list[0] = 1;
     list[1] = 2;
-    list[3] = 3;
+    list[2] = 3;
+
+    // ...
+
+    // Asking the OS for 4 new bytes
+
+    int *tmp = malloc(4 * sizeof(int));
+    if (tmp == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+    tmp[3] = 4;
+
+    // Freeing allocated memory
+    free(list);
+
+    // Make list point to new chunk
+    list = tmp;
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%i\n", list[i]);
+    }
 }
