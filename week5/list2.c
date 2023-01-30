56eecbdba3bf1fb38e573b0307f3c4cb611bf9f7
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-30 17:22:58 +0000

    Mon, Jan 30, 2023, 5:22 PM +00:00

---
+++ week5/list2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number
+    struct node *next;
+}
+node;
+
+int main (void)
+{
+    int *list = malloc(3 * sizeof(int));
+    if (list == NULL)
+    {
+        return 1;
+    }
+
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+
+    // ...
+
+    // Asking the OS for 4 new bytes
+    // Realloc copies the new chunk of bytes
+    int *tmp = realloc(list, 4 * sizeof(int));
+    if (tmp == NULL)
+    {
+        free(list);
+        return 1;
+    }
+    list = tmp;
+
+    list[3] = 4;
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%i\n", list[i]);
+    }
+
+    free(list);
+    return 0;
+}
