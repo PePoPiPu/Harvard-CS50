cebc43d3d57686730bce176cc0127c508807cf56
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-30 14:19:10 +0000

    Mon, Jan 30, 2023, 2:19 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     {
         if ((buffer[3] & 0xf0) == 0xe0)
         {
-            int filename[2];
+            char *c = filename[2];
             sprintf(filename[0], "%03i.jpg", 2);
             FILE *img = fopen(filename, "w");
             fwrite(nimg *ptr, 512, sizeof(card), FILE *jpeg);
