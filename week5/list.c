f5e94500a7377486c5a60c269da174856f5bc891
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-01-30 15:59:54 +0000

    Mon, Jan 30, 2023, 3:59 PM +00:00

--- week5/list.c
+++ week5/list.c
@@ -2,14 +2,9 @@
 
 int main (void)
 {
-    int list[3];
+    int *list = malloc(3 * sizeof(int));
 
-    list [0] = 1;
-    list [1] = 2;
-    list [2] = 3;
-
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%i\n", i);
-    }
+    list[0] = 1;
+    list[1] = 2;
+    list[3] = 3;
 }
