daf751ee1af09b09c220a0ecdae3fff75609ff03
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-01 22:15:31 +0000

    Wed, Feb 1, 2023, 11:15 PM +01:00

--- funpractice/fopen.c
+++ funpractice/fopen.c
@@ -4,10 +4,15 @@
 
 int main (void)
 {
-    FILE *file = fopen("large.txt","r");
+    FILE *file = fopen(large,"r");
     if (file == NULL)
     {
         printf("Could not open file\n");
         return 0;
+    }
+    else
+    {
+        fprintf(file, "Could open file in read mode\n");
+        fclose(file);
     }
 }
