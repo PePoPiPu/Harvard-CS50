a26c9a69b84865d431f1dc0ab2d70cfb4edfdde3
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-01 23:09:02 +0000

    Thu, Feb 2, 2023, 12:09 AM +01:00

---
+++ funpractice/fopen2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// Program that opens a file, and copies each line into a node
+// Definition of node struct
+typedef struct node
+{
+    char word[45 + 1];
+    struct node *next;
+}
+node;
+
+int main (void)
+{
+    // Opens the file and checks for failure/success
+    FILE *file = fopen("large","r");
+    if (file == NULL)
+    {
+        printf("Could not open file\n");
+    }
+    else
+    {
+        printf("Could open file in read mode\n");
+    }
+    // Reads words of the file one by one
+    char word[45];
+    while(fscanf(file, "%s", word) != EOF)
+    {
+        fscanf(file, "%s", word);
+        node *n = malloc(sizeof(node));
+        strcpy
+    }
+    fclose(file);
+}
