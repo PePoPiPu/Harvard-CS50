2032b8faa3bff24c7c8bd02e490dec44ea7ebf65
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-08 18:43:04 +0000

    Wed, Feb 8, 2023, 7:43 PM +01:00

--- funpractice/hash.c
+++ funpractice/hash.c
@@ -58,3 +58,26 @@ HashTable* create_table(int size)
 
     return table;
 }
+
+void free_item (ht_item *item)
+{
+    // Frees an item
+    free(item->key);
+    free(item->value);
+    free(item);
+}
+
+void free_table (HashTable *table)
+{
+    // Frees the table
+    for (int i = 0; i < table->size; i++)
+    {
+        ht_item* item = table->items[i];
+
+        if (item != NULL)
+            free_item(item);
+    }
+
+    free(table->items);
+    free(table);
+}
