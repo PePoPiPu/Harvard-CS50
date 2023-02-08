d85e053158f858c53f389455eba3d0d8236ae4d1
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-08 22:17:10 +0000

    Wed, Feb 8, 2023, 11:17 PM +01:00

--- funpractice/hash.c
+++ funpractice/hash.c
@@ -180,4 +180,16 @@ char* ht_search(HashTable* table, char* key)
     return NULL;
 }
 
-void print_search(HashTable* table, char* key);
+void print_search(HashTable* table, char* key)
+{
+    char* val;
+    if ((val = ht_search(table, key)) == NULL)
+    {
+        printf("Key:%s does not exist\n", key);
+        return;
+    }
+    else
+    {
+        printf("Key:%s, Value:%s\n", key, val);
+    }
+}
