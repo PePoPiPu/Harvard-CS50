c53a134260594a2b21edfc63852189d3958b1863
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-09 21:30:26 +0000

    Thu, Feb 9, 2023, 10:30 PM +01:00

---
+++ funpractice/hashtest.c
@@ -0,0 +1,16 @@
+// Testing of a hash function's distribution
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned int hash(const char *word);
+
+int main (int argc, char *argv[])
+{
+    if (argc < 2)
+        printf("Usage: ./hashtest FILE\n");
+
+    for (int i = 1; i < argc; i++)
+    {}
+}
