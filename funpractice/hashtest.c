e608263425a70d2a3571c5cdfbf0e5beeebb9b6e
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-09 21:49:02 +0000

    Thu, Feb 9, 2023, 10:49 PM +01:00

--- funpractice/hashtest.c
+++ funpractice/hashtest.c
@@ -20,16 +20,16 @@ int main (int argc, char *argv[])
 
 unsigned int hash(const char *word)
 {
-       int l = strlen(word);
-   int ascii = 0;
-   int sum = 0;
-   int hash_value;
+    int l = strlen(word);
+    int ascii = 0;
+    int sum = 0;
+    int hash_value;
 
-   for (int i = 0; i < l; i++)
-   {
+    for (int i = 0; i < l; i++)
+    {
         ascii += word[i];
         sum = ascii + l;
         hash_value = ((sum * word[i]) % 2069) / 100;
-   }
-   return hash_value;
+    }
+    return hash_value;
 }
