994ee994793e726ffbaa9a871b556525e5efb362
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-09 16:28:27 +0000

    Thu, Feb 9, 2023, 5:28 PM +01:00

--- funpractice/hash.c
+++ funpractice/hash.c
@@ -1,72 +1,10 @@
-// Implementation of a hash table and hash function
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 
-#define HASHTABLE_SIZE 10000
-#define LENGTH 45
-
-unsigned int hash(const char *word);
-bool load (const char *large);
-
-typedef struct node
+int main (int argc, char *argv[])
 {
-    char word[LENGTH + 1];
-    struct node *next;
-}
-node;
-// Initializing hashtable
-node *hashtable[HASHTABLE_SIZE];
-
-int word_count = 0;
-
-const unsigned int N = 52;
-
-int main (void)
-{
-
-}
-
-unsigned int hash(const char *word)
-{
+    // Ensure proper usage
+    if (argc < 2)
+        printf("Usage: ./hash WORD\n")
     
 }
-
-bool load (const char *large)
-{
-    FILE *file = fopen(large, "r");
-    if (file == NULL)
-    {
-        printf("Could not load file\n");
-        return false;
-    }
-
-    char word[LENGTH + 1];
-
-    while(fscanf(file, "%s", word) != EOF)
-    {
-        node *new_node = malloc(sizeof(node));
-        if (new_node == NULL)
-            return false;
-
-        strcpy (new_node->word, word);
-        int hash_value = hash(new_node->word);
-        node *head = hashtable[hash_value];
-
-        if (head == NULL)
-        {
-            hastable[hash_value] = new_node;
-            word_count++;
-        }
-        else
-        {
-            new_node->next = hashtable[hash_value];
-            hashtable[hash_value] = new_node;
-            word_count++;
-        }
-
-    }
-}
-
