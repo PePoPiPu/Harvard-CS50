3a358279299172494dac440bb71c6bc1957e7b09
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-09 15:48:03 +0000

    Thu, Feb 9, 2023, 4:48 PM +01:00

--- funpractice/hash.c
+++ funpractice/hash.c
@@ -6,15 +6,49 @@
 #define HASHTABLE_SIZE 10000
 #define LENGTH 45
 
+unsigned int hash(const char *word);
+bool load (const char *large);
+
 typedef struct node
 {
     char word[LENGTH + 1];
     struct node *next;
 }
 node;
-// Initializing hashtable 
+// Initializing hashtable
 node *hashtable[HASHTABLE_SIZE];
 
 int word_count = 0;
 
-const unsigned int N = 52
+const unsigned int N = 52;
+
+int main (void)
+{
+
+}
+
+unsigned int hash(const char *word)
+{
+
+}
+
+bool load (const char *large)
+{
+    FILE *file = fopen(large, "r");
+    if (file == NULL)
+    {
+        printf("Could not load file\n");
+        return false;
+    }
+
+    char word[LENGTH + 1];
+
+    while(fscanf(file, "%s", word) != EOF)
+    {
+        node *new_node = malloc(sizeof(node));
+        if (new_node == NULL)
+            return false;
+
+        
+    }
+}
