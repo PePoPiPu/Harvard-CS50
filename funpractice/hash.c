ae281991877be8aa19cc81472320e338eeb74dba
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-09 16:40:42 +0000

    Thu, Feb 9, 2023, 5:40 PM +01:00

--- funpractice/hash.c
+++ funpractice/hash.c
@@ -1,6 +1,7 @@
 // Program that uses a hash function to print hash values
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 unsigned int hash (const char *word);
 int main (int argc, char *argv[])
 {
@@ -17,5 +18,13 @@ int main (int argc, char *argv[])
 
 unsigned int hash (const char *word)
 {
+   int l = strlen(word);
+   int pos = 0;
+   int sum = 0;
+
+   for (int i = 0; i < l; i++)
+   {
     
+   }
+
 }
