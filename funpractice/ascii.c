2f1971c146b1832a4a1142088070a7f586b9e67b
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-09 17:51:04 +0000

    Thu, Feb 9, 2023, 6:51 PM +01:00

--- funpractice/ascii.c
+++ funpractice/ascii.c
@@ -21,7 +21,7 @@ unsigned int ascii_values (const char *word)
     int ascii = 0;
     for(int i = 0; i < l; i++)
     {
-        ascii = atoi(word);
+        ascii = word[i];
     }
     return ascii;
 }
