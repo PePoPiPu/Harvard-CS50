b31cf876a08af029eff36862211414e7fa7600a5
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-09 16:33:46 +0000

    Thu, Feb 9, 2023, 5:33 PM +01:00

--- funpractice/hash.c
+++ funpractice/hash.c
@@ -1,13 +1,12 @@
 // Program that uses a hash function to print hash values
 #include <stdio.h>
 #include <stdlib.h>
-
+const int hash (const char *word);
 int main (int argc, char *argv[])
 {
     // Ensure proper usage
     if (argc < 2)
         printf("Usage: ./hash WORD\n")
 
-    // For every commad-line argument
-    int h = hash_function
+    int h = hash()
 }
