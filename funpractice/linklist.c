7ad5ff0b1a26570f23a19ea116018f439edfc179
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-10 14:29:18 +0000

    Fri, Feb 10, 2023, 3:29 PM +01:00

---
+++ funpractice/linklist.c
@@ -0,0 +1,16 @@
+// Program that creates a single linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node*
+
+int main (void)
+{
+    
+}
