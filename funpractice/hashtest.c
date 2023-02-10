7d44b39cc00818a376fd4d6b1164918d7fbd5413
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-10 14:02:02 +0000

    Fri, Feb 10, 2023, 3:02 PM +01:00

--- funpractice/hashtest.c
+++ funpractice/hashtest.c
@@ -36,7 +36,7 @@ int main (int argc, char *argv[])
     }
     for (int i = 1; i < argc; i++)
     {
-        int h = hash(dictionary[i]);
+        int h = hash(dictionary);
         printf("%i\n", h);
     }
 }
