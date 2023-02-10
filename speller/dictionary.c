d6924a440493a58779b9e82b2480014223014bb9
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-10 14:44:17 +0000

    Fri, Feb 10, 2023, 3:44 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -98,7 +98,7 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    bool loaded = load(*dictionary);
+    bool loaded = load();
     if(!loaded)
         return 0;
 }
