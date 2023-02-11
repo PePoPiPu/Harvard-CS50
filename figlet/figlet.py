6d4bdd48d216cc84c84409ceae4eebf2b2e7d9d4
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 22:07:13 +0000

    Sat, Feb 11, 2023, 11:07 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,7 +1,9 @@
 import sys
 
 # Ensure proper usage
-if str(sys.argv[1]) != ("-f", "--f"):
+command = "-f"
+command2 = "--f"
+if str(sys.argv[1]) != (command, command2):
     print("Invalid usage")
     sys.exit(1)
 elif len(sys.argv) == 1:
