2ca61e789e35438003f2842473c0dfeae8a6dd6d
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 14:09:45 +0000

    Sat, Feb 11, 2023, 3:09 PM +01:00

---
+++ week6/agree.py
@@ -0,0 +1,7 @@
+s = input("Do you agree? ")
+# Logical "or" and "and" is literally the words instead of || and &&
+if s == "Y" or s == "y":
+    print("Agreed.")
+elif s == "N" or s == "n":
+    printf("Not agreed.")
+#
