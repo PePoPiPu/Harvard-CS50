2d2a43df012b80bbc2c17cf4daf2ec24d87e286b
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 19:43:53 +0000

    Sat, Feb 11, 2023, 8:43 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,6 +4,8 @@ if greeting.startswith("hello"):
     print("$0")
 elif greeting.startswith("hello, "):
     print("$0")
+elif greeting.startswith("hello "):
+    print("$0")
 elif greeting.startswith("h"):
     print("$20")
 else:
