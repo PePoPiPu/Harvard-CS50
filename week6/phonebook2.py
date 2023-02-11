36dbf35bb272df357ccde38c6c6f854a993713e4
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 15:50:27 +0000

    Sat, Feb 11, 2023, 4:50 PM +01:00

---
+++ week6/phonebook2.py
@@ -0,0 +1,11 @@
+# csv files are comma separated values. A form of very lightweight spreadsheet
+import csv
+file = open("phonebook.csv", "a")
+
+name = input("Name: ")
+number = input ("NUmber: ")
+
+writer = csv.writer(file)
+writer.writerow([name, number])
+
+file.close()
