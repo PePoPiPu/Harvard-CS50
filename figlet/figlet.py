959c8817168817e01620c188fae586c728015851
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 21:44:30 +0000

    Sat, Feb 11, 2023, 10:44 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,10 +2,10 @@ import sys
 
 # Ensure proper usage
 if len(sys.argv) == 3:
-    if sys.argv[1] == ("-f", "--f"):
-        print("Arguments are correct")
-    else:
+    if sys.argv[1] != ("-f", "--f"):
         print("Invalid usage")
+    else:
+        print(f"{sys.argv[1]}")
 elif len(sys.argv) == 1:
     s = input("Input: ")
 else:
