7071a4a22ff6a6d19ea82634d84f6985e5ebe7e9
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 13:53:57 +0000

    Sat, Feb 11, 2023, 2:53 PM +01:00

--- week6/calculator.py
+++ week6/calculator.py
@@ -1,6 +1,8 @@
 # The input function gives back a string by default, we have to convert it to an int
 x = int (input ("x: "))
 y = int (input ("y: "))
-
+# In c, dividing an int by an int, it would round down to the nearest integer causing TRUNCATION
+# Python handles the truncation by converting ints automatically to a float
+# A problem that remains
 z = x / y
 print(z)
