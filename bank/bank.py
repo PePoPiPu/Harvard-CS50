5e58851dddcd6854290508411e858b592023db2d
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 19:42:12 +0000

    Sat, Feb 11, 2023, 8:42 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,10 +1,10 @@
 greeting = input("Greeting: ")
 greeting = greeting.lower()
 if greeting.startswith("hello"):
-    print("0$")
+    print("$0")
 elif greeting.startswith("hello, "):
-    print("0$")
+    print("$0")
 elif greeting.startswith("h"):
-    print("20$")
+    print("$20")
 else:
-    print("100$")
+    print("$100")
