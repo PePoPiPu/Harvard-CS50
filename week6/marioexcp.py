ca4087e0d5b1c2d7fc0f562311f837369516a56c
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 15:00:50 +0000

    Sat, Feb 11, 2023, 4:00 PM +01:00

---
+++ week6/marioexcp.py
@@ -0,0 +1,6 @@
+for i in range(4):
+    # As per the documentation, print takes in the name of arguments
+    # Print prints an exit code by default
+    # Override the exit code of the line
+    print("?", end="")
+print()
