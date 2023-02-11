c0e1a39ba81fde25715f40d2476a7622af7602c0
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 21:49:16 +0000

    Sat, Feb 11, 2023, 10:49 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,13 +1,11 @@
 import sys
-
+import argparse
 # Ensure proper usage
-if len(sys.argv) == 3:
-    if sys.argv[1] != ("-f", "--f"):
-        print("Invalid usage")
-    else:
-        print(f"{sys.argv[1]}")
-elif len(sys.argv) == 1:
-    s = input("Input: ")
-else:
-    print("Invalid usage")
-    sys.exit(1)
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-f". "--f")
+
+args = parser.parse_args()
+
+if args.Output:
+    print
