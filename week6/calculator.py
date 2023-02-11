2ca19313b58937720e185bb4a37fabcb360f708a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 13:54:52 +0000

    Sat, Feb 11, 2023, 2:54 PM +01:00

--- week6/calculator.py
+++ week6/calculator.py
@@ -3,6 +3,6 @@ x = int (input ("x: "))
 y = int (input ("y: "))
 # In c, dividing an int by an int, it would round down to the nearest integer causing TRUNCATION
 # Python handles the truncation by converting ints automatically to a float
-# A problem that remains
+# A problem that remains is floating point imprecision
 z = x / y
-print(z)
+print(f"{z:.50f}")
