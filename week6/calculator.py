f59bd49df9bf3f767e055bd420dd5668955621f2
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 13:52:18 +0000

    Sat, Feb 11, 2023, 2:52 PM +01:00

--- week6/calculator.py
+++ week6/calculator.py
@@ -1,5 +1,6 @@
-// The input function gives back a string by default, we have to convert it to an int
+# The input function gives back a string by default, we have to convert it to an int
 x = int (input ("x: "))
 y = int (input ("y: "))
 
-print(x + y)
+z = x / y
+print(z)
