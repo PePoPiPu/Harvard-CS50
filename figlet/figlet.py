ea8e59358ccdbca622b681af187ea33b6313eee8
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 22:43:21 +0000

    Sat, Feb 11, 2023, 11:43 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,6 +2,9 @@ from pyfiglet import Figlet
 import sys, random
 figlet = Figlet()
 figlet.getFonts()
+random.seed()
 
-if len(sys.argv) == 3
+if len(sys.argv) == 3:
     figlet.setFont(font = {sys.argv[2]})
+else:
+    figlet.setFont(random.seed)
