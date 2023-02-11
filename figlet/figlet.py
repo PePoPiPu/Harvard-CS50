bbfa009666cfca34045f31d0985e8f32b82ce0b4
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 21:21:00 +0000

    Sat, Feb 11, 2023, 10:21 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,10 +1,8 @@
 import sys
 
 # Ensure proper usage
-if len(sys.argv) < 2:
-    print("Invalid usage")
-    sys.exit(1)
 if len(sys.argv) < 3:
     print("Invalid usage")
     sys.exit(1)
-
+elif len(sys.argv) = 1:
+    s = input("Input: ")
