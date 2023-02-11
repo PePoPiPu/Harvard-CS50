b5f937e277d5400d60a1c34c84b204e3cc73ecae
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 22:50:32 +0000

    Sat, Feb 11, 2023, 11:50 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,11 +1,6 @@
 from pyfiglet import Figlet
 import sys, random
-figlet = Figlet()
-figlet.getFonts()
-random.seed()
-if len(sys.argv) == 3:
-    figlet.setFont(font = {sys.argv[2]})
-elif len(sys.argv) < 2:
-    figlet.setFont(font = random.seed)
-s = input("Input: ")
-print(figlet.renderText(s))
+if str(sys.argv[1]) == "-f" and "--f":
+    print("it's the same")
+else:
+    print("It's not")
