f6fc1e82795eb32d820a34aa6cc01f44945d6051
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 22:44:48 +0000

    Sat, Feb 11, 2023, 11:44 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,8 +3,9 @@ import sys, random
 figlet = Figlet()
 figlet.getFonts()
 random.seed()
-
 if len(sys.argv) == 3:
     figlet.setFont(font = {sys.argv[2]})
 else:
     figlet.setFont(random.seed)
+s = input("Input: ")
+print(figlet.renderText(s))
