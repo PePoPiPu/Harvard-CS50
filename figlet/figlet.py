853dc2963d3cd6e8b13f50342581b0f4f663044f
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 22:59:02 +0000

    Sat, Feb 11, 2023, 11:59 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,10 +1,10 @@
 from pyfiglet import Figlet
 import sys, random
 
-if (sys.argv[1] == "-f" or sys.argv[1] == "--f"):
+if len(sys.argv) == 3:
+    s = input("Input: ")
+elif (sys.argv[1] == "-f" or sys.argv[1] == "--f"):
     if len(sys.argv) == 3:
         s = input("Input: ")
-elif len(sys.argv) == 1:
-    s = input("input: ")
 else:
     print("Invalid usage")
