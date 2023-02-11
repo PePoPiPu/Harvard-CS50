f97071da8144e8b2366b8ebf67a8f51ff1d68c23
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 21:37:18 +0000

    Sat, Feb 11, 2023, 10:37 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,7 +4,7 @@ import sys
 if len(sys.argv) == 3:
     if sys.argv[1] == ("-f", "--f"):
         s = input("Input: ")
-elif len(sys.argv) == 1:
+if len(sys.argv) == 1:
     s = input("Input: ")
 else:
     print("Invalid usage")
