37fdb3ee983f0526da1e5beba4b31064bfd7ba66
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 21:50:40 +0000

    Sat, Feb 11, 2023, 10:50 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,11 +1,3 @@
 import sys
 import argparse
 # Ensure proper usage
-parser = argparse.ArgumentParser()
-
-parser.add_argument("-f". "--f")
-
-args = parser.parse_args()
-
-if args.Output:
-    print
