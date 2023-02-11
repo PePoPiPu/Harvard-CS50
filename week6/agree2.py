628b8eefeec1cd911bc5772f04d1ccbf5cad97ac
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 14:17:59 +0000

    Sat, Feb 11, 2023, 3:17 PM +01:00

--- week6/agree2.py
+++ week6/agree2.py
@@ -1,7 +1,8 @@
 s = input("Do you agree? ")
 # "If s is in the following list"
-# Python supports object-oriented programming, 
-if s in ["y", "Y"]:
+# Python supports object-oriented programming, certain types of values have attributes and functions built into them
+# Strings come with a lot of functions, to access them we add a . as we were to access a struct in c
+if s.lower() in ["y", "Y"]:
     print("Agreed.")
-elif s in ["N", "n"]:
+elif s.lower() in ["N", "n"]:
     print("Not agreed.")
