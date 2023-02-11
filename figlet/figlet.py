55a3d7ab286f59d2110ff5bc846f777f38cb8c75
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-11 21:09:51 +0000

    Sat, Feb 11, 2023, 10:09 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,5 +1,5 @@
 from sys import argv
 
-    # Ensure proper usage
-if argv is ["-f", "--ff", "FONT_NAME"]:
+# Ensure proper usage
+if argv != ["-f", "--ff", "FONT_NAME"]:
     print("Usage: python figlet.py FONT FONT_NAME")
