2b9e72d0cd67b72d6c05db97d1558595598bb416
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-12 21:50:43 +0000

    Sun, Feb 12, 2023, 10:50 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -29,8 +29,8 @@ def main():
     # TODO: Simulate N tournaments and keep track of win counts
     for i in range(N):
         winner = simulate_tournament(teams)
+        counts.append
         print(f"{winner}")
-    if 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
