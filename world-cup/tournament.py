097af84d27b25bf9d9e5ff1711e4128b3e26fc2c
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-12 22:23:57 +0000

    Sun, Feb 12, 2023, 11:23 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -24,7 +24,7 @@ def main():
             team = {row.get("team"), rating}
             # Append into list
             teams.append(team)
-    print(f"{team}")
+    print(f"{teams[0]}")
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
 
