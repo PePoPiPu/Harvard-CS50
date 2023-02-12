20a3d13206bf5cacd9992d1e6375104adfa6c5cb
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-12 23:21:09 +0000

    Mon, Feb 13, 2023, 12:21 AM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -24,15 +24,13 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    counter = counts[country] = 0
     for i in range(N):
         winner = simulate_tournament(teams)
         country = winner[0]["team"]
-        counter = counts[country] 
-        if country not in counts:
-            counts[country] = 1
-        else:
-            counts[country] =
-    print(f"{country}")
+        
+
+    print(f"{counter}")
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
