d8bc7cae8e656fdea2c6782efc785d09e6aa4c15
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-13 18:52:19 +0000

    Mon, Feb 13, 2023, 7:52 PM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,3 +1,14 @@
 # TODO
-# Prompt user for input
-text = input("Text: ")
+from cs50 import get_string
+def main():
+    # Prompt user for input
+    text = get_string("Text: ")
+    print(f"{text}")
+
+def count_letters():
+    s
+def count_words():
+    s
+def count_sentences():
+    s
+main()
