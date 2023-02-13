5918e7d929978e85309d706fb7f826fcf94ce048
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-13 15:17:56 +0000

    Mon, Feb 13, 2023, 4:17 PM +01:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,10 +1,15 @@
 # TODO
 from cs50 import get_float
+
+
 def main():
+    # Prompting user for input
     owed = get_float("Change owed: ")
+    # Keep prompting if non-negative integer is given
     if owed < 0:
         owed = get_float("Change owed: ")
 
+    # Calculating functions call and rounding up owed variable to avoid floating point imprecision
     quarters = calc_quarters(round(owed, 2))
     owed = owed - (quarters * 0.25)
 
@@ -14,7 +19,7 @@ def main():
     nickels = calc_nickels(round(owed, 2))
     owed = owed - (nickels * 0.05)
 
-    pennies = calc_pennies (round(owed, 2))
+    pennies = calc_pennies(round(owed, 2))
     owed = owed - (pennies * 0.01)
 
     # Sum coins
@@ -22,6 +27,10 @@ def main():
 
     # Print total of coins
     print(f"{coins}")
+
+# Calculate quarters
+
+
 def calc_quarters(owed):
     quarters = 0
     while owed >= 0.25:
@@ -29,18 +38,29 @@ def calc_quarters(owed):
         owed = owed - 0.25
     return quarters
 
+# Calculate dimes
+
+
 def calc_dimes(owed):
     dimes = 0
     while owed >= 0.10:
         dimes += 1
         owed = owed - 0.10
     return dimes
+
+# Calculate nickels
+
+
 def calc_nickels(owed):
     nickels = 0
     while owed >= 0.05:
         nickels += 1
         owed = owed - 0.05
     return nickels
+
+# Calculate pennies
+
+
 def calc_pennies(owed):
     pennies = 0
     while owed >= 0.01:
@@ -48,4 +68,6 @@ def calc_pennies(owed):
         owed = owed - 0.01
     return pennies
 
+
 main()
+
