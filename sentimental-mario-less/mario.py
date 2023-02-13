d1d18b24c52753b4055c2da6e47af98389c303da
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-13 13:22:55 +0000

    Mon, Feb 13, 2023, 2:22 PM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -7,9 +7,13 @@ if s > 8:
 elif s < 1:
     s = get_int("Height: ")
 
-c = 0
-d = 0
+
 for i in range(s):
+    c = 0
+    d = 0
     while c < s - 1:
-        print("#", end = "")
+        print(" ", end = "")
         c += 1
+    while d < s:
+        print("#")
+        d += 1
