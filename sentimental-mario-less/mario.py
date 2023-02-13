2382b5102d716e7d5c0f01a08fa22ad9a1cf5693
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-13 14:15:10 +0000

    Mon, Feb 13, 2023, 3:15 PM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,17 +1,19 @@
 # TODO
 from cs50 import get_int
-
+# Prompt user for input
 while True:
     try:
         h = get_int("Height: ")
-        if h >= 1 and h <=8:
+        if h >= 1 and h <= 8:
             break
     except:
         h = get_int("Height: ")
-
-for i in range (h):
+# Loop through height range
+for i in range(h):
+    # Print spaces
     for s in range(h-i-1):
-        print(" ", end = "")
+        print(" ", end="")
+    # Print hashes
     for j in range(i + 1):
-        print("#", end = "")
+        print("#", end="")
     print()
