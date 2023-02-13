9dd77428e25c2cd023e631e0f7d658a06c936ecb
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-13 14:40:05 +0000

    Mon, Feb 13, 2023, 3:40 PM +01:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,9 +1,17 @@
 # TODO
 from cs50 import get_float
-
-owed = get_float("Change owed: ")
-if owed > 0:
-    print(f"Change owed: ${owed:.2f}")
-else:
+def main():
     owed = get_float("Change owed: ")
-    print(f"Change owed: ${owed:.2f}")
+    if owed > 0:
+        print(f"Change owed: ${owed:.2f}")
+    else:
+        owed = get_float("Change owed: ")
+        print(f"Change owed: ${owed:.2f}")
+
+def quarters(owed):
+    quarters = 0
+    while owed >= 25:
+        quarters += 1
+
+        owed = owed - 25
+main()
