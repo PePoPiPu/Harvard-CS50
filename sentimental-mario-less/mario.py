3ece31fe8da5e32c38b5518e77f70bee54012702
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-13 13:14:22 +0000

    Mon, Feb 13, 2023, 2:14 PM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,3 +1,4 @@
+# TODO
 from cs50 import get_int
 
 s = get_int("Height: ")
@@ -5,9 +6,8 @@ if s > 8:
     s = get_int("Height: ")
 elif s < 1:
     s = get_int("Height: ")
-j = s
+
+# For height
 for i in range(s):
-    while j = range - 1
-# TODO
-# for i in range s
-# print i - 1 spaces
+    # print s - 1 spaces
+    
