062924709dca7771539be968b796531e551c6d81
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-14 19:20:53 +0000

    Tue, Feb 14, 2023, 8:20 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -10,8 +10,7 @@ def main():
     subsequence = []
     # TODO: Read database file into a variable
     with open(sys.argv[1], "r") as csvFile:
-        reader = csv.reader(csvFile)
-        subreader = next(reader)
+        reader = csv.DictReader(csvFile)
         subsequence = subreader[1:]
     # TODO: Read DNA sequence file into a variable
     with open(sys.argv[2], "r") as sequences:
