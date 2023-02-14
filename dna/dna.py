4df075a4198a25a68832b166cfb57863259f484a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-14 15:10:35 +0000

    Tue, Feb 14, 2023, 4:10 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -13,7 +13,7 @@ def main():
         data_reader = csv.DictReader(databases)
     # TODO: Read DNA sequence file into a variable
     with open(sys.argv[2], "r") as sequences:
-        seq_reader = csv.DictReader(sequences)
+        f.read(sequences)
     # TODO: Find longest match of each STR in DNA sequence
     print(f"{data_reader}")
     # TODO: Check database for matching profiles
