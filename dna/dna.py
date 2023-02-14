c5859933335a420d4f209e6e5366660f5e6f2a4b
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-14 14:53:48 +0000

    Tue, Feb 14, 2023, 3:53 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,17 +5,17 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-if len(sys.argv) != 3:
-    print("Usage: dna.py CSV FILE TEXT FILE")
+    if len(sys.argv) != 3:
+        print("Usage: dna.py CSV FILE TEXT FILE")
 
     # TODO: Read database file into a variable
-with open("databases.txt", "r") as databases
-reader = csv.DictReader(database)
+    with open(sys.argv[1], "r") as databases:
+        reader = csv.DictReader(databases)
     # TODO: Read DNA sequence file into a variable
-with open("sequences.txt", "w") as sequences
-reader = csv.DictReader(sequences)
+    with open("sequences.txt", "w") as sequences:
+        reader = csv.DictReader(sequences)
     # TODO: Find longest match of each STR in DNA sequence
-match = longest_match(sequences)
+    match = longest_match(sequences)
     # TODO: Check database for matching profiles
 
 
