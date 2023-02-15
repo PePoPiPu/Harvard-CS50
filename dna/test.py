f71fddc11048a896d7715cd5ee658be2b835640f
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-15 15:04:08 +0000

    Wed, Feb 15, 2023, 4:04 PM +01:00

--- dna/test.py
+++ dna/test.py
@@ -2,13 +2,15 @@ import sys
 import csv
 
 match = {"AGATC": 29, "TTTTTTCT":50, "AATG":18, "TCTAG":23, "GATA":38, "TATC":24, "GAAA":22, "TCTG":9}
+match_list = match.items()
 with open(sys.argv[1], "r") as csvFile:
     reader = csv.DictReader(csvFile)
     for row in reader:
         del row["name"]
-    if row.items() == match.items():
+    row_list = row.items()
+    if row_list == match_list:
         print("Equal")
     else:
         print("Different")
-print(row.items())
-print(match.items())
+print(row_list)
+print(match_list)
