93fd6f262919fec973e19331c02aac8d68b46c4c
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-15 10:14:34 +0000

    Wed, Feb 15, 2023, 11:14 AM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -19,11 +19,11 @@ def main():
     # TODO: Find longest match of each STR in DNA sequence. Load it into dict
     for i in range(8):
         matches = {subsequence[i]: longest_match(sequence, subsequence[i])}
-        print(f"{matches}")
+        print(f"{reader.fieldnames[0]}")
     # TODO: Check database for matching profiles
-        for i in range(len(csvFile)):
-            if matches == row["name"]:
-                print(f"{reader.fieldnames[:0]}")
+        for i in range(len(reader)):
+            if matches == reader.fieldnames[1:8]:
+                print(f"{reader.fieldnames[0]}")
             else:
                 print("No match.")
 
