fbf3c6dbef34f595f63e8f72aec0ce5f5c4ebd8d
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-15 09:59:58 +0000

    Wed, Feb 15, 2023, 10:59 AM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -21,7 +21,7 @@ def main():
         matches = {subsequence[i]: longest_match(sequence, subsequence[i])}
         print(f"{matches}")
     # TODO: Check database for matching profiles
-    for row in reader:
+    for name in reader:
         if matches = row:
             print(f"{reader.fieldnames[:0]}")
         else:
