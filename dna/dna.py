f338b644fb2cacd607cc5ad5008d79106c055686
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-15 15:28:14 +0000

    Wed, Feb 15, 2023, 4:28 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -23,15 +23,21 @@ def main():
         # Convert match values to int
     for value in match.values():
         match[subsequence[i]] = int(value)
-    print(type(match))
     # TODO: Check database for matching profiles
     with open(sys.argv[1], "r") as csvFile:
         reader = csv.DictReader(csvFile)
         for row in reader:
+            # Delete first column in row
             del row["name"]
-            for value in row.values():
-                row[value] = int(value)
-        print(row)
+            # Convert row values to int
+            for i in range(8):
+                for value in row.values():
+                    row[subsequence[i]] = int(value)
+            # Compare row values to match values
+            if row == match:
+                print("Found")
+            else:
+                continue
 
 
 def longest_match(sequence, subsequence):
