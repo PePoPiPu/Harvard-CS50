23103453eff376b56afebad3ff142b301d33902b
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-15 15:15:40 +0000

    Wed, Feb 15, 2023, 4:15 PM +01:00

--- dna/test.py
+++ dna/test.py
@@ -4,4 +4,8 @@ import csv
 match = {"AGATC": 29, "TTTTTTCT":50, "AATG":18, "TCTAG":23, "GATA":38, "TATC":24, "GAAA":22, "TCTG":9}
 with open(sys.argv[1], "r") as csvFile:
     reader = csv.DictReader(csvFile)
-    for row in reader
+
+
+for item in match:
+    match = int(item)
+print(match)
