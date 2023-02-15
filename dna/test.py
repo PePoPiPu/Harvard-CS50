d45bc608a57729277a43b3d3b0cb2bcf3f7929a5
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-15 15:14:16 +0000

    Wed, Feb 15, 2023, 4:14 PM +01:00

--- dna/test.py
+++ dna/test.py
@@ -2,21 +2,6 @@ import sys
 import csv
 
 match = {"AGATC": 29, "TTTTTTCT":50, "AATG":18, "TCTAG":23, "GATA":38, "TATC":24, "GAAA":22, "TCTG":9}
-match_list = match.items()
-match_listint = [dict([key, int(value)]
-       for key, value in dicts.items())
-       for dicts in match_list]
 with open(sys.argv[1], "r") as csvFile:
     reader = csv.DictReader(csvFile)
-    for row in reader:
-        del row["name"]
-    row_list = row.items()
-    row_listint =[dict([key, int(value)]
-            for keu, value in dicts.items())
-            for dicts in ]
-    if row_list == match_list:
-        print("Equal")
-    else:
-        print("Different")
-print(row_list)
-print(match_listint)
+    for row in reader
