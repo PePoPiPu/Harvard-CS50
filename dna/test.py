2ab562b43ae6874bd0cbc8a090366fda0f65957a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-15 15:05:30 +0000

    Wed, Feb 15, 2023, 4:05 PM +01:00

--- dna/test.py
+++ dna/test.py
@@ -3,6 +3,9 @@ import csv
 
 match = {"AGATC": 29, "TTTTTTCT":50, "AATG":18, "TCTAG":23, "GATA":38, "TATC":24, "GAAA":22, "TCTG":9}
 match_list = match.items()
+for dicts in match_list:
+    for keys in dicts:
+        dicts[keys] = int(dicts[keys])
 with open(sys.argv[1], "r") as csvFile:
     reader = csv.DictReader(csvFile)
     for row in reader:
