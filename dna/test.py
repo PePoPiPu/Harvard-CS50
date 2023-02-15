f5af2f54ae2340996dca824707aaf7d9d1373af7
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-15 14:48:42 +0000

    Wed, Feb 15, 2023, 3:48 PM +01:00

--- dna/test.py
+++ dna/test.py
@@ -1,12 +1,14 @@
-match = {"AGATC": 0, "TTTTTTCT":0, "AATG":0, "TCTAG":0, "GATA":0, "TATC":0, "GAAA":0, "TCTG":0}
+import sys
+import csv
 
-match["AGATC"] = longest_match(sequence, subsequence[0])
-match["TTTTTTCT"] = longest_match(sequence, subsequence[1])
-match["AATG"] = longest_match(sequence, subsequence[2])
-match["TCTAG"] = longest_match(sequence, subsequence[3])
-match["GATA"] = longest_match(sequence, subsequence[4])
-match["TATC"] = longest_match(sequence, subsequence[5])
-match["GAAA"] = longest_match(sequence, subsequence[6])
-match["TCTG"] = longest_match(sequence, subsequence[7])
-
-match[subsequence[i]] = longest_match(sequence, subsequence[i])
+match = {"AGATC": 29, "TTTTTTCT":50, "AATG":18, "TCTAG":23, "GATA":38, "TATC":24, "GAAA":22, "TCTG":9}
+with open(sys.argv[1], "r") as csvFile:
+    reader = csv.DictReader(csvFile)
+    for row in reader:
+        del row["name"]
+    if row == match:
+        print("Equal")
+    else:
+        print("Different")
+print(row)
+print(match)
