5f25b5f5c7c130ad350b60b6af0f2fa07441801d
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-15 16:10:19 +0000

    Wed, Feb 15, 2023, 5:10 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -35,8 +35,7 @@ def main():
             # Compare row values to match values
             if row_int == match:
                 print(name)
-            else:
-                continue
+            
 
 def longest_match(sequence, subsequence):
     """Returns length of longest run of subsequence in sequence."""
