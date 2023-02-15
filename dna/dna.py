2d5b8caae7df4a61ca50dd7598aef87380c011ab
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-15 12:58:34 +0000

    Wed, Feb 15, 2023, 1:58 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -25,7 +25,7 @@ def main():
             matches_list.append(match.values())
     # TODO: Check database for matching profiles
         for row in reader:
-            if match in row:
+            if row == match:
                 print("Found")
 
 
