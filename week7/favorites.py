2e72dfac8ab9fa4712273b95487e2432b26f76c5
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-16 15:32:23 +0000

    Thu, Feb 16, 2023, 4:32 PM +01:00

--- week7/favorites.py
+++ week7/favorites.py
@@ -1,7 +1,7 @@
 import csv
 
 with open("favorites.csv" , "r") as file:
-    reader = csv.reader(file)
+    reader = csv.DictReader(file)
     for row in reader:
-        favorite = row[1]
+        favorite = row["language"]
         print(favorite)
