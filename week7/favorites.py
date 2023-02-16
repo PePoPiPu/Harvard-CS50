6cb75336fe4dde54720ecee54f9a1ff1b45a9ec8
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-16 15:37:02 +0000

    Thu, Feb 16, 2023, 4:37 PM +01:00

--- week7/favorites.py
+++ week7/favorites.py
@@ -2,6 +2,15 @@ import csv
 
 with open("favorites.csv" , "r") as file:
     reader = csv.DictReader(file)
+    scratch, c, python = 0, 0, 0
     for row in reader:
         favorite = row["language"]
-        print(favorite)
+        if favorite == "Scratch":
+            scratch += 1
+        elif favorite == "C":
+            c += 1
+        elif favorite == "Python":
+            python += 1
+    print(f"Scratch: {scratch}")
+    print(f"Python : {python}")
+    print(f"C: {c}")
