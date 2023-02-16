9145fc87529f64815d7abfd134e3ef25d7aaf79e
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-16 18:07:12 +0000

    Thu, Feb 16, 2023, 7:07 PM +01:00

--- week7/favorites.py
+++ week7/favorites.py
@@ -3,7 +3,7 @@ from cs50 import SQL
 db = SQL("sqlite:///favorites.db")
 
 favorite = input("Favorite: ")
-
+# Returns list of dictionaries when using SELECT
 rows = db.execute("SELECT * FROM favorites WHERE problem = 'Mario'")
 
 for row in rows:
