e47704d6c06829efa409f68cbc6ae7aad0099009
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-17 23:02:43 +0000

    Sat, Feb 18, 2023, 12:02 AM +01:00

--- prophecy/roster.py
+++ prophecy/roster.py
@@ -3,6 +3,8 @@ from cs50 import SQL
 db = SQL("sqlite:///roster.db")
 with open("students.csv", "r") as students:
     reader = csv.reader(students)
-    for row in students:
-        # db.execute("INSERT INTO students (student_id, student_name) VALUES (?)" , row)
-        print(row[])
+    # Skip header row
+    next(reader)
+    for row in reader:
+        db.execute("INSERT INTO student (student_id, student_name) VALUES (?, ?)" , row[0], row[1])
+        print(row[0])
