4c99b65bf0772324c11cba3c429787020eeda24f
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-17 22:00:20 +0000

    Fri, Feb 17, 2023, 11:00 PM +01:00

---
+++ prophecy/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE student (
+    student_id INTEGER,
+    student_name TEXT,
+    FOREIGN KEY(house_id)
+);
+
+CREATE TABLE house (
+    house_id INTEGER,
+    house_name TEXT,
+    PRIMARY KEY(house_id)
+);
+
+CREATE TABLE head (
+    head_id INTEGER,
+    head_name TEXT,
+    FOREIGN KEY(house_id)
+);
