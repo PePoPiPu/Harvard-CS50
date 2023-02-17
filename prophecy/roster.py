8000aef593d9326b5729ce3d3ee72e09cc974bdc
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-17 22:53:46 +0000

    Fri, Feb 17, 2023, 11:53 PM +01:00

--- prophecy/roster.py
+++ prophecy/roster.py
@@ -4,4 +4,4 @@ db = SQL("sqlite:///roster.db")
 with open("students.csv", "r") as students:
     reader = csv.reader(students)
     for row in students:
-        db.execute("INSERT INTO students)
+        db.execute("INSERT INTO students (student_id, student_name) VALUES 
