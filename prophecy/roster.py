e5fa2f86961e6606bc1c2e25a514238aa838a2d9
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-17 23:12:49 +0000

    Sat, Feb 18, 2023, 12:12 AM +01:00

--- prophecy/roster.py
+++ prophecy/roster.py
@@ -6,7 +6,6 @@ with open("students.csv", "r") as students:
     # Skip header row
     next(reader)
     for row in reader:
-        db.execute("INSERT INTO student (student_id, student_name) VALUES (?, ?)" , row[0], row[1])
+        db.execute("DELETE FROM student (student_id, student_name) VALUES (?, ?)" , row[0], row[1])
         db.execute("INSERT INTO house (house_name) VALUES (?)", row[2])
         db.execute("INSERT INTO head (head_name) VALUES (?)", row[3])
-        print(row[0])
