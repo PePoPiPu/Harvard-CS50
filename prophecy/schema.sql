4449f5485d9d942e6afa789f611aaff0ceaa2808
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-18 12:56:23 +0000

    Sat, Feb 18, 2023, 1:56 PM +01:00

--- prophecy/schema.sql
+++ prophecy/schema.sql
@@ -12,9 +12,9 @@ CREATE TABLE houses (
 );
 
 CREATE TABLE assignments (
-    student_id INTEGER,
-    head_name TEXT,
-    house_id INTEGER,
+    student_id INTEGER NOT NULL,
+    head_name TEXT NOT NULL,
+    house_id INTEGER NOT NULL,
     FOREIGN KEY(student_id) REFERENCES students(id),
     FOREIGN KEY (head_name) REFERENCES houses(head_name),
     FOREIGN KEY (house_id) REFERENCES house(id)
