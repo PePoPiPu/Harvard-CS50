d64c8c14edd79aa3de33ca4a8b0f36b663f0ba4d
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-18 22:42:14 +0000

    Sat, Feb 18, 2023, 11:42 PM +01:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,2 +1,1 @@
-SELECT rating FROM ratings WHERE movie_id =
-SELECT id FROM movies WHERE year = '2012';
+SELECT AVG(ratings.rating) FROM raitings JOIN movies ON ratings.movie_id = movies.id WHERE movies.year = '2012';
