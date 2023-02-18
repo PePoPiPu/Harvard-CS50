c189711e40ce41d3dfb5eb52b96b068ceb86c109
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-18 23:06:57 +0000

    Sun, Feb 19, 2023, 12:06 AM +01:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,3 @@
-SELECT ratings.rating FROM ratings JOIN movies ON ratings.movie_id = movies.id WHERE movies.year = '2010' ORDER BY ratings.rating DESC;;
+SELECT ratings.rating FROM ratings JOIN movies ON ratings.movie_id = movies.id WHERE movies.year = '2010' ORDER BY ratings.rating DESC;
+
+SELECT 
