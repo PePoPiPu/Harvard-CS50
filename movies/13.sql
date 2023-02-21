fed3da3bfb662741af12fd8d905fb7664c86ceec
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-21 22:55:26 +0000

    Tue, Feb 21, 2023, 11:55 PM +01:00

--- movies/13.sql
+++ movies/13.sql
@@ -18,3 +18,14 @@ WHERE stars.movie_id IN
 (SELECT movies.id FROM movies
 WHERE people.name = 'Kevin Bacon'
 AND people.birth = '1958'));
+
+
+SELECT people.name FROM people
+JOIN stars ON people.id = stars.people_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE stars.person_id IN
+(SELECT people.id FROM people
+WHERE people.name IN
+(SELECT people.id FROM people
+WHERE movies.title IN
+(SELECT movies.title FROM movies)))
