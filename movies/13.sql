a0df7a5f9e97a4c81c4184f647475b6e6b3d3481
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-21 23:14:56 +0000

    Wed, Feb 22, 2023, 12:14 AM +01:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,4 @@
 SELECT stars.person_id FROM stars
-JOIN stars ON people.id = stars.person_id
+JOIN people ON stars.person_id = people.id
 WHERE people.name = 'Kevin Bacon'
 AND people.birth = '1958';
