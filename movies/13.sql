6658e4eaa95fa45db0989afa29ab3b51feaa6370
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-21 23:04:27 +0000

    Wed, Feb 22, 2023, 12:04 AM +01:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,33 +1,5 @@
 SELECT people.name FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-
-SELECT stars.person_id FROM stars
+WHERE people.id IN
+(SELECT stars.person_id FROM stars
 WHERE stars.movie_id IN
-(SELECT movies.id FROM movies
-WHERE people.name = 'Kevin Bacon'
-AND people.birth = '1958');
-
-
-SELECT people.name FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE people.name IN
-(SELECT movies.title FROM movies
-WHERE stars.movie_id IN
-(SELECT movies.id FROM movies
-WHERE people.name = 'Kevin Bacon'
-AND people.birth = '1958'));
-
-Test code
-SELECT people.name FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE stars.person_id IN
-(SELECT people.id FROM people
-WHERE people.name IN
-(SELECT people.id FROM people
-WHERE movies.title IN
-(SELECT movies.title FROM movies
-WHERE people.name = 'Kevin Bacon'
-AND people.birth = '1958')));
+(SELECT stars.movie_id))
