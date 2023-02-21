3973c2daf06305c01bbedabfb8af1bee2bd9d96d
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-21 23:05:37 +0000

    Wed, Feb 22, 2023, 12:05 AM +01:00

--- movies/13.sql
+++ movies/13.sql
@@ -2,4 +2,5 @@ SELECT people.name FROM people
 WHERE people.id IN
 (SELECT stars.person_id FROM stars
 WHERE stars.movie_id IN
-(SELECT stars.movie_id))
+(SELECT stars.movie_id
+WHERE stars.person_id ))
