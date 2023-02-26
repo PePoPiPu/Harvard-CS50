eec4b4383661c3f8123b15fed9c65f7ba0b6fa9d
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-26 19:09:46 +0000

    Sun, Feb 26, 2023, 8:09 PM +01:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,8 +1,8 @@
-SELECT people.name FROM people
-WHERE people.id IN
-(SELECT people.id FROM people
-WHERE people.id IN
-(SELECT stars.movie_id FROM stars
-JOIN people ON stars.person_id = people.id
-WHERE people.name = 'Kevin Bacon'
-AND people.birth = '1958'));
+SELECT distinct(name) FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.title IN(
+SELECT distinct(movies.title) FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE people.name = "Kevin Bacon" AND people.birth = 1958) AND people.name != "Kevin Bacon";
