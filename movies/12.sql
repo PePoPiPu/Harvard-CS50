615a3cc2ca2514fd6a76c1771efa0de48be22c25
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-26 18:52:16 +0000

    Sun, Feb 26, 2023, 7:52 PM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,5 +2,3 @@ SELECT movies.title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 WHERE people.id IN
-(SELECT stars.person_id FROM stars
-WHERE people.name = 'Johnny Depp');
