c89237018331b1156c4f30d73910e1c9915672ff
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-02-26 18:59:00 +0000

    Sun, Feb 26, 2023, 7:59 PM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -3,7 +3,5 @@ JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 WHERE people.id IN
 (SELECT people.id FROM people
-WHERE people.name = 'Johnny Depp')
-AND people.id IN
-(SELECT people.id FROM people
-WHERE people.name = 'Helena Bonham Carter')
+WHERE people.name IN
+('Johnny Depp', 'Helena Bonham Carter'));
