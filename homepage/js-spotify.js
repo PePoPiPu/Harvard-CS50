93563900d4e1078d6560d4111c1659ea1760b5b1
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-08 16:21:26 +0000

    Wed, Mar 8, 2023, 5:21 PM +01:00

--- homepage/js-spotify.js
+++ homepage/js-spotify.js
@@ -1,6 +1,8 @@
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
     let element = document.getElementById('embed-iframe');
     let options = {
+        width: '50%',
+        height: '400',
         uri: 'spotify:artist:4Z8W4fKeB5YxbusRsdQVPb'
         };
     let callback = (EmbedController) => {};
