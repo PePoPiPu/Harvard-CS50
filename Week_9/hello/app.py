8ccc8e1d43646837992c6ced992a39fa4c648cd5
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-11 10:08:01 +0000

    Sat, Mar 11, 2023, 11:08 AM +01:00

--- Week_9/hello/app.py
+++ Week_9/hello/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__) #__name__ refers to the current file. "Turn this file into
 @app.route("/") #The @ is called a decorator
 def index():
     if "name" in request.args:
-        name= request.args["names"]
+        name= request.args["name"]
     else:
         name="world"
     return render_template("index.html")
