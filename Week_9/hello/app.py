00356cc646a7a2d4c51934e2574d67da013689f2
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-11 10:02:14 +0000

    Sat, Mar 11, 2023, 11:02 AM +01:00

--- Week_9/hello/app.py
+++ Week_9/hello/app.py
@@ -1,3 +1,8 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__) #__name__ refers to the current file. "Turn this file into a Flask application"
+
+
+@app.route("/") #The @ is called a decorator
+def index():
+    return "hello, world"
