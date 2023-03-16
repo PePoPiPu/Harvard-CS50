1c0934f9789cab39b165a6e27a667c7706a7c60c
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-16 00:00:59 +0000

    Thu, Mar 16, 2023, 1:00 AM +01:00

--- froshims/app.py
+++ froshims/app.py
@@ -1,41 +1,48 @@
-# Implements a registration form, storing registrants in a dictionary, with error messages
+# Implements a registration form, storing registrants in a SQLite database, with support for deregistration
 
+from cs50 import SQL
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-REGISTRANTS = {}
+db = SQL("sqlite:///froshims.db")
+
 SPORTS = [
     "Basketball",
     "Soccer",
     "Ultimate Frisbee"
 ]
 
-app.route("/")
+@app.route("/")
 def index():
     return render_template("index.html", sports=SPORTS)
 
-app.route("/register", methods=["POST"])
+@app.route("/deregister", methods=["POST"])
+def deregister():
+
+    # Forget registrant
+    id = request.form.get("id")
+    if id:
+        db.execute("DELETE FROM registrants WHERE id = ?", id)
+    return redirect("/registrants")
+
+@app.route("/register", methods=["POST"])
 def register():
 
-    # Validate name
+    # Validate submission
     name = request.form.get("name")
-    if not name:
-        return render_template("error.html", message="Missing name")
-
-    # Validate sport
     sport = request.form.get("sport")
-    if not sport:
-        return render_template("error.html", message="Missing sport")
-    if sport not in SPORTS:
-        return render_template("error.html", message="Invalid sport")
+    if not name or sport not in SPORTS:
+        return render_template("failure.html")
 
     # Remember registrant
-    REGISTRANTS[name] = sport
+    db.execute("INSERT INTO  registrants (name, sport) VALUES(?, ?)", name, sport)
 
     # Confirm registration
     return redirect("/registrants")
 
+
 @app.route("/registrants")
 def registrants():
-    return render_template("registrants.html", registrants=REGISTRANTS)
+    registrants = db.execute("SELECT * FROM registrants")
+    return render_template("registrants.html", registrants=registrants)
