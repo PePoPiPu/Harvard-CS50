cc23a596174a35c1dd99ecc8b2e2d41c2c430543
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-20 22:55:17 +0000

    Mon, Mar 20, 2023, 11:55 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -234,12 +234,14 @@ def sell():
         # Update user's balance
         db.execute("UPDATE users SET cash = ? WHERE id = ?", updated_cash, id)
         # Update stocks table
-        # Substract number of sahres sold to current shares if it's more than 0 and update the database
-
-        old_shares = db.execute("SELECT shares_number FROM stocks")
-        current_shares = int(request.form.get("shares") - int(old_shares))
-        if shares = 0:
+        # Substract number of shares sold to current shares if it's more than 0 and update the database
+        if shares > 1:
+            old_shares = db.execute("SELECT shares_number FROM stocks")
+            current_shares = int(request.form.get("shares") - int(old_shares))
             db.execute("UPDATE stocks SET shares_number = ?", current_shares)
+        else:
+            db.execute("DELETE FROM stocks WHERE stock_symbol = ?", symbol)
+
         return redirect("/")
     else:
         rows = db.execute("SELECT share_symbol FROM stocks")
