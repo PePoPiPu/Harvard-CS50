b57ab9103f663d4b766b06bbb7af2deda4c6d6ab
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-22 12:18:20 +0000

    Wed, Mar 22, 2023, 1:18 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -109,7 +109,12 @@ def buy():
 
                 # Updating transactions table
                 transaction_checker = db.execute("SELECT 1 FROM transactions bought WHERE share_symbol = ? AND user_id = ?", symbol, id)
-                
+                if transaction_checker == 1:
+                    # Update transactions table
+                    db.execute("UPDATE transactions SET bought = bought WHERE id = :id", id=session["user_id"])
+                else:
+                    db.execute("INSERT INTO transactions )
+
             else:
                 db.execute("INSERT INTO stocks (user_id, shares_number, share_symbol, time_of_purchase, value_at_time_of_purchase) VALUES(?, ?, ?, ?, ?)", id, int(request.form.get("shares")), symbol["symbol"], date, init_value)
 
