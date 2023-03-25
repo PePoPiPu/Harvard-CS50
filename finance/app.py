9022409ac8c4af59b92f66d015fdf079fc01757b
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-25 16:05:19 +0000

    Sat, Mar 25, 2023, 5:05 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -79,6 +79,8 @@ def buy():
             return apology("Couldn't find stock")
         elif len(request.form.get("shares")) == 0:
             return apology("Must provide a number of shares to sell")
+        elif isinstance(request.form.get("shares")) == True:
+            return apology("Input must be a number")
         elif int(request.form.get("shares")) < 1:
             return apology("Share number must be greater than 0")
         # Retrieving current cash amount
