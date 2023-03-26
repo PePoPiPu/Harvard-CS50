458edb76085a8e767642aafbb69eca2f372abb47
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-26 23:49:50 +0000

    Mon, Mar 27, 2023, 1:49 AM +02:00

--- Final Project/app.py
+++ Final Project/app.py
@@ -2,7 +2,7 @@ import os
 
 # Importing modules
 from cs50 import SQL
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
@@ -10,6 +10,21 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configuring the app
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 library to use SQLite database
+db = SQL("sqlite:///psp.db")
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/")
 def index():
