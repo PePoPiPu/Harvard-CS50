b5eee64ded3e15a82476a941943c6e28ed9484aa
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-26 20:36:09 +0000

    Sun, Mar 26, 2023, 10:36 PM +02:00

--- Final Project/app.py
+++ Final Project/app.py
@@ -2,7 +2,7 @@ import os
 
 # Importing modules
 from cs50 import SQL
-from flask import Flask
+from flask import Flask, render_template
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
@@ -10,3 +10,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configuring the app
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
