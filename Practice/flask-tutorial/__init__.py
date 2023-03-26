1171d3fc000313b6d1ac1598e83fddd6cba0d6e5
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-26 21:49:19 +0000

    Sun, Mar 26, 2023, 11:49 PM +02:00

--- Practice/flask-tutorial/__init__.py
+++ Practice/flask-tutorial/__init__.py
@@ -2,9 +2,18 @@ import os
 
 from flask import Flask
 
+# Creating the application factory function
 def create_app(test_config=None):
     # Create and configure the app
-    app = Flask(__name__, instance_realative_config=True)
+
+    # Creating the Flask instance
+        # Name is the name of the current python module
+
+        # Instance_relative_config=True tells the app that configuration files are relative
+        # to the instance folder, located outside the flaskr package
+    app = Flask(__name__, instance_relative_config=True)
+
+    # Sets some default configuration
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
