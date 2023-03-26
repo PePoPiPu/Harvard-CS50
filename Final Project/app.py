8289f1822fe636255fe35ec9471f4498d9344fa7
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-26 20:31:27 +0000

    Sun, Mar 26, 2023, 10:31 PM +02:00

--- Final Project/app.py
+++ Final Project/app.py
@@ -1,7 +1,12 @@
 import os
+
+# Importing modules
 from cs50 import SQL
 from flask import Flask
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configuring the app
+app = Flask(__name__)
