8869939f9ed493b0c45d4bdc97b798c611e08b04
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-30 14:49:59 +0000

    Thu, Mar 30, 2023, 4:49 PM +02:00

---
+++ Final Project/tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE staff (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT NOT NULL,
+    seniority TEXT NOT NULL,
+    performance TEXT NOT NULL
+    shift TEXT NOT NULL,
+    
+)
