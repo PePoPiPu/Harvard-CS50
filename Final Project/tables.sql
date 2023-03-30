cdfe363cd7dee250085e48c71eaef819fab1c3c3
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-30 14:54:59 +0000

    Thu, Mar 30, 2023, 4:54 PM +02:00

--- Final Project/tables.sql
+++ Final Project/tables.sql
@@ -2,7 +2,16 @@ CREATE TABLE staff (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT NOT NULL,
     seniority TEXT NOT NULL,
-    performance TEXT NOT NULL
-    shift TEXT NOT NULL,
-    
+    performance_level TEXT NOT NULL,
+    shift TEXT NOT NULL
+);
+
+CREATE TABLE counter (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    counter_name TEXT NOT NULL,
+    selling_point TEXT NOT NULL,
+    level TEXT NOT NULL,
+    morning_service_hours TEXT NOT NULL,
+    late_service_hours TEXT NOT NULL,
+    night_service_hours TEXT NOT NULL
 )
