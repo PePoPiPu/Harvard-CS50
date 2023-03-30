65aa2754cc1a153010f7a867759832793f6842f8
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-03-30 14:59:59 +0000

    Thu, Mar 30, 2023, 4:59 PM +02:00

--- Final Project/tables.sql
+++ Final Project/tables.sql
@@ -14,4 +14,18 @@ CREATE TABLE counter (
     morning_service_hours TEXT NOT NULL,
     late_service_hours TEXT NOT NULL,
     night_service_hours TEXT NOT NULL
+);
+
+CREATE TABLE schedule (
+    active_counters INTEGER NOT NULL,
+    active_staff INTEGER NOT NULL,
+    shift TEXT NOT NULL,
+    date TEXT NOT NULL,
+    weekday TEXT NOT NULL,
+    FOREIGN KEY (active_counters),
+    REFERENCES counter (id)
+    FOREIGN KEY (active_staff),
+    REFERENCES staff (id),
+    FOREIGN KEY (shift),
+    REFERENCES staff (shift)
 )
