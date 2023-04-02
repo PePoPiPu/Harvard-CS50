89630d51990a688e53e3333db28a6dbc44498dd5
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-02 15:09:23 +0000

    Sun, Apr 2, 2023, 5:09 PM +02:00

--- Final Project/app.py
+++ Final Project/app.py
@@ -19,7 +19,7 @@ Session(app)
 
 # Configure CS50 library to use SQLite database
 db = SQL("sqlite:///psp.db")
- 
+
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -142,3 +142,7 @@ def register():
     # Reached route via GET
     else:
         return render_template("register.html")
+
+@app.route("/staff", methods=["GET", "POST"])
+@login_required
+def staff():
