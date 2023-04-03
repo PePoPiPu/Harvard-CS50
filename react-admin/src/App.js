1ef1a4ce5d3de2742f394049334e708f30cf4421
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-03 17:32:47 +0000

    Mon, Apr 3, 2023, 7:32 PM +02:00

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,8 +1,19 @@
+import { ColorModeContext, useMode } from './theme';
+impor { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-    </div>
-  );
+    <ColorModeContext.Provide value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provide>
+    );
 }
 
 export default App;
