3631865502ab7c11f09f830349eb273ca264b0db
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-04 21:26:51 +0000

    Tue, Apr 4, 2023, 11:26 PM +02:00

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from ".scenes/global/team";
+import Team from ".scenes/global/team";
 // import Invoices from ".scenes/global/invoices";
 // import Contacts from ".scenes/global/contacts";
 // import Bar from ".scenes/global/bar";
@@ -28,7 +28,7 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} /> */}
+              <Route path="/team" element={<Team />} />
               {/* <Route path="/contacts" element={<Contacts />} /> */}
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
