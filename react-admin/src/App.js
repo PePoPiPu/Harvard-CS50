3a44712c3f23339c5a0257d8f2dcf5f321864d80
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-04 21:46:51 +0000

    Tue, Apr 4, 2023, 11:46 PM +02:00

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-import Team from ".scenes/global/team";
+import Team from "./scenes/team";
 // import Invoices from ".scenes/global/invoices";
 // import Contacts from ".scenes/global/contacts";
 // import Bar from ".scenes/global/bar";
