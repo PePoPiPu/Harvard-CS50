a6c9c6c7210dd50d42cd79c8881763fed15b5ef4
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-06 09:21:57 +0000

    Thu, Apr 6, 2023, 11:21 AM +02:00

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -7,8 +7,8 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
+import Form from "./scenes/global/form";
 // import Bar from "./scenes/global/bar";
-// import Form from "./scenes/global/form";
 // import Line from "./scenes/global/line";
 // import Pie from "./scenes/global/pie";
 // import FAQ from "./scenes/global/faq";
@@ -31,7 +31,7 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/form" element={<Form />} /> */}
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
