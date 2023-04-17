bb46ac5611ff2b05044db8fd1f7781570e47e359
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-17 09:35:47 +0000

    Mon, Apr 17, 2023, 11:35 AM +02:00

--- react-planner/src/App.js
+++ react-planner/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import sidebar from "./scenes/global/sidebar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
