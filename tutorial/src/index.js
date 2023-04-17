8587890548c33a3311a83c0a886b6042a7286e93
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-17 09:59:07 +0000

    Mon, Apr 17, 2023, 11:59 AM +02:00

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -6,8 +6,7 @@ class Square extends React.Component {
     render() {
       return (
         <button className="square" onClick={function () {
-            console.log('click');
-        }}>
+            console.log('click'); }}>
           {this.props.value}
         </button>
       );
