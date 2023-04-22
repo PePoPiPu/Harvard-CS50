9c73d92656d4c041936e101fe55a47fe81087e0a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-22 17:36:37 +0000

    Sat, Apr 22, 2023, 7:36 PM +02:00

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { WebGL } from 'three/addons/capabilities/WebGL.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -23,4 +24,9 @@ function animate() {
 	renderer.render( scene, camera );
 }
 
-animate();
+if ( WebGL.isWebGLAvailable() ) {
+    animate();
+} else {
+    const warning = WebGl.getWebGLErrorMessage();
+    document.getElementByUd( 'container' ).appendChild( warning );
+}
