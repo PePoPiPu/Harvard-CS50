6c2319865f325b716d8df68bf477ef9b81a774a6
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-22 14:06:38 +0000

    Sat, Apr 22, 2023, 4:06 PM +02:00

--- my-app/src/Components/NoteComponents/Notes.js
+++ my-app/src/Components/NoteComponents/Notes.js
@@ -1,4 +1,5 @@
 import { React, useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 import "../css/Note.css";
 import Note from "./Note";
 import CreateNote from "./CreateNote";
@@ -7,7 +8,31 @@ function Notes() {
     // states
     const [notes, setNotes] = useState([]);
     const [inputText, setInputText] = useState("")
-    
+
+    // get text and store in state
+    const textHandler = (e) => {
+        setInputText(e.target.value);
+    };
+
+    // add new note to the state array
+    const saveHandler = () => {
+        setNotes((prevState) => [
+            ...prevState,
+            {
+                id: uuid(),
+                text: inputText,
+            },
+        ]);
+        // clear the textarea
+        setInputText("");
+    };
+
+    // delete note function
+    const deleteNote = (id) => {
+        const filteredNotes = notes.filter((note) => note.id !== id);
+        setNotes(filteredNotes);
+    };
+
     return (
         <div className="notes">
             <Note />
