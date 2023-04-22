e68846c774c023f8c973ea31110b27a9d612b55c
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-22 17:38:42 +0000

    Sat, Apr 22, 2023, 7:38 PM +02:00

--- app/main.js
+++ app/main.js
@@ -24,14 +24,4 @@ function animate() {
 	renderer.render( scene, camera );
 }
 
-if ( WebGL.isWebGLAvailable() ) {
-
-	// Initiate function or other initializations here
-	animate();
-
-} else {
-
-	const warning = WebGL.getWebGLErrorMessage();
-	document.getElementById( 'container' ).appendChild( warning );
-
-}
+animate();
