fbe024bd0179b8f1ebaba4fca0380cbc49f9551f
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-23 14:44:20 +0000

    Sun, Apr 23, 2023, 4:44 PM +02:00

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -5,6 +5,22 @@ myImage.onclick = () => {
     if (mySrc === "images/firefox-icon.png") {
         myImage.setAttribute("src", "images/firefox2.png");
     } else {
-        myImage.setAttribute("src", "images/firefox2.png");
+        myImage.setAttribute("src", "images/firefox-icon.png");
     }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUsername () {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUsername();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
