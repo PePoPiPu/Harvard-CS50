a6ed64b2bece3653817a7d60148bf1bcc52908ea
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-04-25 09:17:30 +0000

    Tue, Apr 25, 2023, 11:17 AM +02:00

--- tests/testing/src/App.js
+++ tests/testing/src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import { FiberContainer } from'./FiberContainer'
+import { FiberContainer } from './FiberContainer';
 
 function App() {
-  return (
-    <div className="App">
+  return <div className="App">
       <FiberContainer />
     </div>
-  );
 }
 
 export default App;
