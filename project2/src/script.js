e88a7db77526e88679798613ddb660f65f43056a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-05-13 23:08:30 +0000

    Sun, May 14, 2023, 1:08 AM +02:00

--- project2/src/script.js
+++ project2/src/script.js
@@ -135,8 +135,8 @@ const gui = new dat.GUI();
 const params = {
   galaxySize: 100,
   armCount: 6,
-  baseColor: '#FFC864',
-  centerColor: '#6496FF'
+  baseColor: '#6496FF',
+  centerColor: '#FFC864'
 };
 
 // Add controls to the GUI
