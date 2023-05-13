ee0e151c6ebb2420ecfabf8cf23b63c241ce048b
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-05-13 21:00:50 +0000

    Sat, May 13, 2023, 11:00 PM +02:00

--- project2/src/script.js
+++ project2/src/script.js
@@ -1,99 +1,79 @@
-import './styles.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import './styles.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000)
-camera.position.z = 5
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+camera.position.z = 1500;
 
 // Renderer
-const renderer = new THREE.WebGLRenderer({ canvas: canvas })
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.setPixelRatio(window.devicePixelRatio);
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x000000); // Set clear color to black
-camera.aspect = window.innerWidth / window.innerHeight;
-camera.updateProjectionMatrix();
 
 // Controls
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // Geometry
-const galaxySize = 1000
-const points = []
+const galaxySize = 1000;
+const points = [];
 
-for (let i = 0; i < 10000; i++) {
-  const angle = Math.random() * Math.PI * 2
-  const distance = Math.random() * galaxySize
-  const height = Math.random() * 2 - 1
+const numPoints = 10000;
+const spiralTurns = 10;
+const spiralHeight = 2;
+const spiralRadius = 10;
 
-  const x = Math.cos(angle) * distance
-  const y = height * galaxySize
-  const z = Math.sin(angle) * distance
+for (let i = 0; i < numPoints; i++) {
+  const t = i / numPoints;
+  const angle = spiralTurns * 2 * Math.PI * t;
+  const height = (t * 2 - 1) * spiralHeight;
+  const radius = spiralRadius * t;
 
-  points.push(new THREE.Vector3(x, y, z))
+  const x = Math.cos(angle) * radius;
+  const y = height * galaxySize;
+  const z = Math.sin(angle) * radius;
+
+  points.push(new THREE.Vector3(x, y, z));
 }
 
-const geometry = new THREE.BufferGeometry().setFromPoints(points)
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
 // Colors
-const colors = []
-const color1 = new THREE.Color(0x8000ff) // Starting color
-const color2 = new THREE.Color(0x00ff00) // Ending color
+const colors = [];
+
+const colorStart = new THREE.Color(0x8000ff); // Starting color
+const colorEnd = new THREE.Color(0x00ff00); // Ending color
 
 for (let i = 0; i < points.length; i++) {
-  const t = i / points.length
-  const color = new THREE.Color().lerpColors(color1, color2, t)
-  colors.push(color.r, color.g, color.b)
+  const t = i / points.length;
+  const color = new THREE.Color().lerpColors(colorStart, colorEnd, t);
+  colors.push(color.r, color.g, color.b);
 }
 
-geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
+geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
 
 // Material
-const vertexShader = `
-  varying vec3 vColor;
-
-  void main() {
-    vColor = color;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  }
-`
-
-const fragmentShader = `
-  varying vec3 vColor;
-
-  void main() {
-    gl_FragColor = vec4(vColor, 1.0);
-  }
-`
-
-const material = new THREE.ShaderMaterial({
-  vertexShader,
-  fragmentShader,
-  vertexColors: true,
-})
+const material = new THREE.PointsMaterial({ vertexColors: true });
 
 // Mesh
-const galaxy = new THREE.Points(geometry, material)
-scene.add(galaxy)
-galaxy.position.set(0, 0, 0);
+const galaxy = new THREE.Points(geometry, material);
+scene.add(galaxy);
 
 // Animation loop
 function animate() {
-  requestAnimationFrame(animate)
+  requestAnimationFrame(animate);
 
   // Rotate the galaxy
-  galaxy.rotation.y += 0.001
+  galaxy.rotation.y += 0.001;
 
   // Render the scene
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 }
 
 // Start the animation loop
-animate()
+animate();
