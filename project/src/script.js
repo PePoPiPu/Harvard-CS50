b9366ea592e26905bd3566942b2eed5b8e0ed01a
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-05-13 18:02:10 +0000

    Sat, May 13, 2023, 8:02 PM +02:00

--- project/src/script.js
+++ project/src/script.js
@@ -139,45 +139,7 @@ let geometry = new THREE.BufferGeometry().setFromPoints(points)
 // const loadedTexture = new THREE.PointsMaterial({ map:texture })
 
 // Creating a shader
-var material = new THREE.ShaderMaterial({
-    uniforms: {
-      color1: {
-        value: new THREE.Color("red")
-      },
-      color2: {
-        value: new THREE.Color("purple")
-      },
-      bboxMin: {
-        value: geometry.boundingBox.min
-      },
-      bboxMax: {
-        value: geometry.boundingBox.max
-      }
-    },
-    vertexShader: `
-      uniform vec3 bboxMin;
-      uniform vec3 bboxMax;
-
-      varying vec2 vUv;
-
-      void main() {
-        vUv.y = (position.y - bboxMin.y) / (bboxMax.y - bboxMin.y);
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
-      }
-    `,
-    fragmentShader: `
-      uniform vec3 color1;
-      uniform vec3 color2;
-
-      varying vec2 vUv;
-
-      void main() {
-
-        gl_FragColor = vec4(mix(color1, color2, vUv.y), 1.0);
-      }
-    `,
-    wireframe: true
-  });
+ 
 const spiralGalaxy = new THREE.Points(geometry, material)
 scene.add(spiralGalaxy)
 
