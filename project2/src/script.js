c665ada7b21088ca99dd6ced51a367c276f20733
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-05-13 22:07:26 +0000

    Sun, May 14, 2023, 12:07 AM +02:00

--- project2/src/script.js
+++ project2/src/script.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
