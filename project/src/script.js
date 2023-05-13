e82db6b0db3fa09947f9a895793de21b8794d093
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-05-13 19:30:40 +0000

    Sat, May 13, 2023, 9:30 PM +02:00

--- project/src/script.js
+++ project/src/script.js
@@ -1,10 +1,9 @@
 import './styles.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
 
 // Debug
-const gui = new dat.GUI()
+
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
