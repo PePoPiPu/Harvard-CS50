f08628f86f238319966277e034eb85dbaa8ce9f3
alexsugimoto <119201937+alexsugimoto@users.noreply.github.com>
2023-05-14 11:43:04 +0000

    Sun, May 14, 2023, 1:43 PM +02:00

--- project2/src/script.js
+++ project2/src/script.js
@@ -220,9 +220,9 @@ gui.listen();
 
 // Make text disappear on click
 window.addEventListener('click', function() {
-  var allElements = document.querySelectorAll('*');
+  var textElements = document.querySelectorAll('p, h1');
 
-  for (var i = 0; i , allElements.length; i++) {
-    allElements[i].style.display = 'none'
+  for (var i = 0; i < textElements.length; i++) {
+    textElements[i].style.display = 'none';
   }
 });
